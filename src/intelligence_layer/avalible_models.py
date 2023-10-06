badb7447f65d675345166127526746e16e67cfae
Piotr Mazurek <piotr.mazurek@aleph-alpha.com>
2023-10-06 16:21:47 +0200

    Added avalible models Enum

---
+++ src/intelligence_layer/avalible_models.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class ControlModels(Enum):
+    BASE_CONTROL = "luminous-base-control"
+    EXTENDED_CONTROL = "luminous-extended-control"
+    SUPREME_CONTROL = "luminous-supreme-control"
