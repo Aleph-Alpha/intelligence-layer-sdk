09086ed45b604834adcef29f22817abc3c9bc6b6
Piotr Mazurek <piotr.mazurek@aleph-alpha.com>
2023-10-09 11:27:13 +0200

    Renam to account for a dif between a doc and chunk

--- src/intelligence_layer/multiple_chunk_qa.py
+++ src/intelligence_layer/multiple_chunk_qa.py
@@ -1,10 +1,6 @@
-from intelligence_layer.single_chunk_qa import (
-    SingleChunkQaInput,
-    QaOutput,
-    SingleChunkQa,
-    TextRange,
-)
+from intelligence_layer.qa import SingleDocumentQaInput, QaOutput, SingleChunkQa
 from intelligence_layer.task import DebugLog, LogLevel, Task
+from intelligence_layer.avalible_models import ControlModels
 from aleph_alpha_client import (
     Client,
     CompletionRequest,
@@ -13,18 +9,15 @@ from aleph_alpha_client import (
     Prompt,
     TextScore,
 )
-from intelligence_layer.completion import Completion, CompletionInput, CompletionOutput
-from typing import List, Tuple
+from typing import List
 from pydantic import BaseModel
 
-
-class MultipleChunkQaInput(BaseModel):
-    chunks: List[str]
-    question: str
+class MultipleDocumentQaInput(BaseModel):
+    documents = List[SingleDocumentQaInput]
 
 
-class MultipleChunkQa(Task[MultipleChunkQaInput, QaOutput]):
-    PROMPT_TEMPLATE = """### Instruction:
+class MultipleDocumentQa(Task[SingleDocumentQaInput, QaOutput]):
+    PROMPT_TEMPLATE="""### Instruction:
 You will be given a number of Answers to a Question. Based on them, generate a single final answer.
 Condense multiple answers into a single answer. Rely only on the provided answers. Don't use the world's knowledge. The answer should combine the individual answers. If the answers contradict each other, e.g., one saying that the colour is green and the other saying that the colour is black, say that there are contradicting answers saying the colour is green or the colour is black.
 
@@ -41,36 +34,22 @@ Final answer:"""
         self,
         client: Client,
         log_level: LogLevel,
-        model: str = "luminous-supreme-control",
+        model: ControlModels = ControlModels.SUPREME_CONTROL,
     ):
-        self.client = client
-        self.log_level = log_level
-        self.completion = Completion(client, log_level)
-        self.single_chunk_qa = SingleChunkQa(client, log_level, model)
 
-    def _prompt_text(
-        self, text: str, question: str, no_answer_text: str
-    ) -> Tuple[str, TextRange]:
-        prompt = self.PROMPT_TEMPLATE.format(
-            question=question, no_answer_text=no_answer_text
-        )
-        # need to calculate the range differently
-        return prompt, TextRange(start=len(prompt), end=len(prompt) + len(text))
+        
+        self.single_document_qa = SingleChunkQa(client, log_level, model)
+    
 
-    def run(self, input: MultipleChunkQaInput) -> QaOutput:
+    def run(self, input: MultipleDocumentQaInput) -> QaOutput:
         """Executes the process for this use-case."""
+        
+        qa_outputs: List[QaOutput] = [self.single_document_qa.run(doc) for doc in input.documents]
 
-        qa_outputs: List[QaOutput] = [
-            self.single_chunk_qa.run(
-                SingleChunkQaInput(question=input.question, chunk=chunk)
-            )
-            for chunk in input.chunks
-        ]
+        answers: List[str] = [output.answer for output in qa_outputs]
 
-        answers: List[str | None] = [output.answer for output in qa_outputs]
+        completion = Completion(client, log_level)
+        
 
-        return QaOutput(
-            answer="XXX",
-            highlights=["xxxx", "zzzz"],
-            debug_log=DebugLog.enabled(level=self.log_level),
-        )
+
+
