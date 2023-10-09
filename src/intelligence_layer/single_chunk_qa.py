62af065d9a79f32277a3796f4e674d866d348116
Johannes Wesch <johannes.wesch@aleph-alpha.com>
2023-10-09 16:32:04 +0200

    fixed the naming for multiple_chunk_qa

--- src/intelligence_layer/single_chunk_qa.py
+++ src/intelligence_layer/single_chunk_qa.py
@@ -55,7 +55,7 @@ class SingleChunkQa(Task[SingleChunkQaInput, QaOutput]):
         self,
         client: Client,
         log_level: LogLevel,
-        model: str = "luminous-supreme-control"
+        model: str = "luminous-supreme-control",
     ):
         self.client = client
         self.log_level = log_level
@@ -147,11 +147,12 @@ class SingleChunkQa(Task[SingleChunkQaInput, QaOutput]):
 
 if __name__ == "__main__":
     import os
+
     token = os.getenv("AA_TOKEN")
     client = Client(token=token)
 
     qa = SingleChunkQa(client, "info")
-    
+
     input = SingleChunkQaInput(
         chunk="Paul Nicolas lost his mother at the age of 3, and then his father in 1914.[3] He was raised by his mother-in-law together with his brother Henri. He began his football career with Saint-Mandé Club in 1916. Initially, he played as a defender, but he quickly realized that his destiny laid at the forefront since he scored many goals.[3] In addition to his goal-scoring instinct, Nicolas also stood out for his strong character on the pitch, and these two qualities combined eventually drew the attention of Mr. Fort, the then president of the Gallia Club, who signed him as a centre-forward in 1916.",
         question="What is the name of Paul Nicolas' brother?",
--- tests/test_single_chunk_qa.py
+++ tests/test_single_chunk_qa.py
@@ -24,7 +24,7 @@ def test_qa_with_answer(qa: SingleChunkQa) -> None:
 def test_qa_with_no_answer(qa: SingleChunkQa) -> None:
     input = SingleChunkQaInput(
         chunk="Paul Nicolas lost his mother at the age of 3, and then his father in 1914.[3] He was raised by his mother-in-law together with his brother Henri. He began his football career with Saint-Mandé Club in 1916. Initially, he played as a defender, but he quickly realized that his destiny laid at the forefront since he scored many goals.[3] In addition to his goal-scoring instinct, Nicolas also stood out for his strong character on the pitch, and these two qualities combined eventually drew the attention of Mr. Fort, the then president of the Gallia Club, who signed him as a centre-forward in 1916.",
-        question="What is the capital of Germany?"
+        question="What is the capital of Germany?",
     )
     output = qa.run(input)
 
