22610fa9f47fb39fa9c193a2750063ce59d4de70
Johannes Wesch <johannes.wesch@aleph-alpha.com>
2023-10-10 14:50:23 +0200

    fixed test

--- tests/test_multiple_chunk_qa.py
+++ tests/test_multiple_chunk_qa.py
@@ -44,7 +44,7 @@ def test_multiple_chunk_qa_with_mulitple_chunks(qa: MultipleChunkQa) -> None:
 
     chunks = [
         "Paul Nicolas lost his mother at the age of 3, and then his father in 1914.[3] He was raised by his mother-in-law together with his brother Henri. He began his football career with Saint-Mandé Club in 1916. Initially, he played as a defender, but he quickly realized that his destiny laid at the forefront since he scored many goals.[3]",
-        "In addition to his goal-scoring instinct, Nicolas also stood out for his strong character on the pitch, and these two qualities combined eventually drew the attention of Mr. Fort, the then president of the Gallia Club, who signed him as a centre-forward in 1916.",
+        "In addition to his goal-scoring instinct, Nicolas also stood out for his strong character on the pitch, and these two qualities combined eventually drew the attention of Mr. Fort, the then president of the Gallia Club, who signed him as a centre-forward in 1916. ",
     ]
 
     input = MultipleChunkQaInput(chunks=chunks, question=question)
