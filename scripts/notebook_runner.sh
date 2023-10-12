261d0d7c6e7c0b0414cbda765158e16d02fdfda6
pitneitemeier <pit.neitemeier@aleph-alpha.com>
2023-10-12 15:16:09 +0200

    fixed loading of token

--- scripts/notebook_runner.sh
+++ scripts/notebook_runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#AA_TOKEN=$(echo $AA_TOKEN) 
-source .env
+# next line loads AA_TOKEN from .env file when running bash script locally. In CI this is not necessary since AA_TOKEN is environment variable.
+[ -f .env ] && source .env
 AA_TOKEN=$(echo $AA_TOKEN) jupyter nbconvert --to notebook --execute src/intelligence_layer/*.ipynb
 rm src/intelligence_layer/*.nbconvert.ipynb
