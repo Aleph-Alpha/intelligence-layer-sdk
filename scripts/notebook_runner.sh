3807dd3d59194569d3d71fc572f963322267644a
Rutger Cappendijk <rutger.cappendijk@aleph-alpha.com>
2023-10-18 14:46:13 +0200

    added max workers for poetry install to silence CI warnings

--- scripts/notebook_runner.sh
+++ scripts/notebook_runner.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env -S bash -eu -o pipefail
 # next line loads AA_TOKEN from .env file when running bash script locally. In CI this is not necessary since AA_TOKEN is environment variable.
 [ -f .env ] && source .env
+poetry config installer.max-workers 10
 AA_TOKEN="$AA_TOKEN" jupyter nbconvert --to notebook --execute src/examples/*.ipynb
 rm src/examples/*.nbconvert.ipynb
