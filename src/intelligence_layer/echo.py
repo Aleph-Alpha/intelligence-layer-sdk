ea5ce68fc931257e3d63672eec227d2b2091eeaa
Johannes Wesch <johannes.wesch@aleph-alpha.com>
2023-10-24 15:00:44 +0200

    v1 of echo task

--- src/intelligence_layer/echo.py
+++ src/intelligence_layer/echo.py
@@ -1,6 +1,9 @@
 from typing import NewType, Sequence
-from aleph_alpha_client import Prompt
+from aleph_alpha_client import Client, CompletionRequest, Prompt, TokenizationRequest
 from pydantic import BaseModel
+import tokenizers
+from intelligence_layer.completion import RawCompletion, RawCompletionInput
+from intelligence_layer.prompt_template import PromptTemplate
 
 from intelligence_layer.task import DebugLogger, Task
 
@@ -25,13 +28,82 @@ class TokenWithProb(BaseModel):
 
 
 class EchoInput(BaseModel):
-    prompt: Prompt
+    prompt: str
+    expected_completion: str
+    model: str
 
 
 class EchoOutput(BaseModel):
-    tokens_with_log_probs: Sequence[Token]
+    tokens_with_log_probs: Sequence[TokenWithProb]
 
 
 class EchoTask(Task[EchoInput, EchoOutput]):
+    """Task that returns probabilities of the completion based on the given model and prompt.
+
+    This task asks the model how likely the given completion is. It does not generate any tokens.
+
+    Args:
+        client: Aleph Alpha client instance for running model related API calls.
+
+    Example:
+
+    """
+
+    def __init__(self, client: Client) -> None:
+        super().__init__()
+        self._completion = RawCompletion(client=client)
+        self._client = client
+
     def run(self, input: EchoInput, logger: DebugLogger) -> EchoOutput:
-        return EchoOutput()
+        prompt = Prompt.from_text(input.prompt + input.expected_completion)
+        completion_input = RawCompletionInput(
+            request=self._completion_request(prompt=prompt),
+            model=input.model,
+        )
+
+        output = self._completion.run(completion_input, logger)
+        tokens = self._tokenize_expected_completion(
+            input.expected_completion, input.model
+        )
+        log_prob_dicts = output.response.completions[0].log_probs[-len(tokens) :][0]
+
+        tokens_with_prob = []
+        for token, log_prob in zip(tokens, log_prob_dicts.values()):
+            assert log_prob is not None
+            tokens_with_prob.append(
+                TokenWithProb(
+                    token=token,
+                    prob=LogProb(log_prob),
+                )
+            )
+
+        return EchoOutput(tokens_with_log_probs=tokens_with_prob)
+
+    def _completion_request(
+        self,
+        prompt: Prompt,
+    ) -> CompletionRequest:
+        return CompletionRequest(
+            prompt=prompt,
+            maximum_tokens=0,
+            log_probs=0,
+            tokens=True,
+            echo=True,
+        )
+
+    def _tokenize_expected_completion(
+        self, expected_output: str, model: str
+    ) -> Sequence[Token]:
+        """Turns th expected output into list of token ids. Important so that we know how many tokens
+        the label is and can retrieve the last N log probs for the label"""
+        response = self._client.tokenize(
+            request=TokenizationRequest(
+                expected_output + "<|endoftext|>", tokens=True, token_ids=True
+            ),
+            model=model,
+        )
+        assert response.token_ids and response.tokens
+        return [
+            Token(token=token, token_id=token_id)
+            for token, token_id in zip(response.tokens, response.token_ids)
+        ]
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -1,2 +1,46 @@
-def test_can_run_echo_task() -> None:
-    task = Echo
+from typing import Sequence
+from aleph_alpha_client import Client, Prompt, TokenizationRequest
+from pytest import fixture
+import tokenizers
+
+from intelligence_layer.echo import EchoInput, EchoTask, Token, TokenWithProb
+from intelligence_layer.task import NoOpDebugLogger
+
+
+@fixture
+def echo_task(client: Client) -> EchoTask:
+    return EchoTask(client)
+
+
+def tokenize_completion(
+    expected_output: str, model: str, client: Client
+) -> Sequence[Token]:
+    """Turns th expected output into list of token ids. Important so that we know how many tokens
+    the label is and can retrieve the last N log probs for the label"""
+    response = client.tokenize(
+        request=TokenizationRequest(
+            expected_output + "<|endoftext|>", tokens=True, token_ids=True
+        ),
+        model=model,
+    )
+    assert response.token_ids and response.tokens
+    return [
+        Token(token=token, token_id=token_id)
+        for token, token_id in zip(response.tokens, response.token_ids)
+    ]
+
+
+def test_can_run_echo_task(echo_task: EchoTask) -> None:
+    excpected_completion = "good."
+    input = EchoInput(
+        prompt="The weather is",
+        expected_completion=excpected_completion,
+        model="luminous-base",
+    )
+    tokens = tokenize_completion(excpected_completion, input.model, echo_task._client)
+
+    result = echo_task.run(input, logger=NoOpDebugLogger())
+
+    assert all([isinstance(t, TokenWithProb) for t in result.tokens_with_log_probs])
+    for token, result_token in zip(tokens, result.tokens_with_log_probs):
+        assert token == result_token.token
