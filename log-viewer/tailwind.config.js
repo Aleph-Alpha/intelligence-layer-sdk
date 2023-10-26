3b51792dda90adfa38828bc4a33c24e6c3850077
Ben Brandt <benjamin.j.brandt@gmail.com>
2023-10-26 21:03:03 +0200

    Bring in theme settings from KW UI

--- log-viewer/tailwind.config.js
+++ log-viewer/tailwind.config.js
@@ -1,8 +1,42 @@
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			black: colors.black,
+			gray: colors.gray,
+			red: colors.red,
+			white: colors.white,
+			accent: {
+				DEFAULT: '#e3ff00',
+				50: '#ffffe4',
+				100: '#feffc4',
+				200: '#fbff90',
+				300: '#f2ff50',
+				400: '#e3ff00',
+				500: '#c7e600',
+				600: '#9ab800',
+				700: '#748b00',
+				800: '#5b6d07',
+				900: '#4d5c0b',
+				950: '#283400'
+			}
+		},
+		extend: {
+			fontFamily: {
+				sans: ['Raleway', ...defaultTheme.fontFamily.sans],
+				mono: ['iA Writer Mono V', ...defaultTheme.fontFamily.mono]
+			}
+		}
 	},
-	plugins: []
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/container-queries')
+	]
 };
