f7de9880ccc76dcc4fd56447ba509ac83a3a7d01
niklas <niklas.finken@aleph-alpha.com>
2023-10-27 15:50:52 +0200

    ClassifyEvaluator can evaluate all classifiers

--- src/intelligence_layer/use_cases/classify/classify.py
+++ src/intelligence_layer/use_cases/classify/classify.py
@@ -1,9 +1,12 @@
 from typing import (
     Mapping,
+    Sequence,
 )
 
 from pydantic import BaseModel
-from intelligence_layer.core.task import Chunk, Probability
+from intelligence_layer.core.evaluator import Evaluator
+from intelligence_layer.core.logger import DebugLogger
+from intelligence_layer.core.task import Chunk, Probability, Task
 
 
 class ClassifyInput(BaseModel):
@@ -29,3 +32,74 @@ class ClassifyOutput(BaseModel):
     """
 
     scores: Mapping[str, Probability]
+
+
+class Classify(Task[ClassifyInput, ClassifyOutput]):
+    """Placeholder class for any classifier implementation."""
+
+    pass
+
+
+class ClassifyEvaluation(BaseModel):
+    """The evaluation of a single label classification run.
+
+    Attributes:
+        correct: Was the highest scoring class from the output in the set of "correct classes"
+        output: The actual output from the task run
+    """
+
+    correct: bool
+    output: ClassifyOutput
+
+
+class AggregatedClassifyEvaluation(BaseModel):
+    """The aggregated evaluation of a single label classify implementation against a dataset.
+
+    Attributes:
+        percentage_correct: Percentage of answers that were considered to be correct
+        evaluation: The actual evaluations
+    """
+
+    percentage_correct: float
+    evaluations: Sequence[ClassifyEvaluation]
+
+
+class ClassifyEvaluator(
+    Evaluator[
+        ClassifyInput,
+        Sequence[str],
+        ClassifyEvaluation,
+        AggregatedClassifyEvaluation,
+    ]
+):
+    def __init__(self, task: Classify):
+        self.task = task
+
+    def evaluate(
+        self,
+        input: ClassifyInput,
+        logger: DebugLogger,
+        expected_output: Sequence[str],
+    ) -> ClassifyEvaluation:
+        output = self.task.run(input, logger)
+        sorted_classes = sorted(
+            output.scores.items(), key=lambda item: item[1], reverse=True
+        )
+        if sorted_classes[0][0] in expected_output:
+            correct = True
+        else:
+            correct = False
+        return ClassifyEvaluation(correct=correct, output=output)
+
+    def aggregate(
+        self, evaluations: Sequence[ClassifyEvaluation]
+    ) -> AggregatedClassifyEvaluation:
+        if len(evaluations) != 0:
+            correct_answers = len(
+                [eval.correct for eval in evaluations if eval.correct == True]
+            ) / len(evaluations)
+        else:
+            correct_answers = 0
+        return AggregatedClassifyEvaluation(
+            percentage_correct=correct_answers, evaluations=evaluations
+        )
