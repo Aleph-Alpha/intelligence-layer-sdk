01289c16ec282d52ef1fa4bc76c6eb0d887f9df8
Ben Brandt <benjamin.j.brandt@gmail.com>
2023-10-27 16:02:14 +0200

    INtegrated timeline

---
+++ log-viewer/src/lib/components/timeline/Timeline.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { DebugLog } from '../../log';
+	import LogDurations from './LogDurations.svelte';
+	import SpanTree from './SpanTree.svelte';
+
+	/**
+	 * The Debug Log you want to render
+	 */
+	export let log: DebugLog;
+</script>
+
+<!--
+    @component
+    Timeline and Tree view of the given DebugLog
+-->
+
+<div class="grid grid-cols-3 grid-rows-1">
+	<div class="col-span-1">
+		<SpanTree logs={log.logs} />
+	</div>
+	<div class="col-span-2"><LogDurations logs={log.logs} /></div>
+</div>
