4f63fa46fe8b2b100dd2d2158ff4666ff334d16e
Volker Stampa <Volker.Stampa@aleph-alpha.com>
2023-10-30 16:03:21 +0100

    Run nb in parallel

--- scripts/notebook_runner.sh
+++ scripts/notebook_runner.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env -S bash -eu -o pipefail
 # next line loads AA_TOKEN from .env file when running bash script locally. In CI this is not necessary since AA_TOKEN is environment variable.
 [ -f .env ] && source .env
-AA_TOKEN="$AA_TOKEN" jupyter nbconvert --to notebook --execute src/examples/*.ipynb
-rm src/examples/*.nbconvert.ipynb
+# Find all .ipynb files in the directory and pass them to xargs for parallel execution
+rm --force --recursive src/examples/*.nbconvert.ipynb src/examples/.ipynb_checkpoints
+find src/examples/ -name "*.ipynb" | xargs -I {} --max-procs 1 bash -c "AA_TOKEN=\"$AA_TOKEN\" poetry run jupyter nbconvert --to notebook --execute {}"
+rm --force src/examples/*.nbconvert.ipynb
