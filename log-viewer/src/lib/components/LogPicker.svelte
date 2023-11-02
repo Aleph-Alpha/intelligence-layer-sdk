b9f5c48c7c22db88611b39ee75937d24ca22feb6
Volker Stampa <Volker.Stampa@aleph-alpha.com>
2023-11-02 14:41:50 +0100

    Add log-file upload to log picker

--- log-viewer/src/lib/components/LogPicker.svelte
+++ log-viewer/src/lib/components/LogPicker.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { activeLog } from '$lib/active';
+	import { parseLogFile } from '$lib/logfile.parser';
 
+	let files: FileList;
 	// Reset on refresh
 	let value = '';
 </script>
@@ -14,13 +16,22 @@
 		>Upload a debug log to render
 	</label>
 	<textarea
-		class="appearance-none border-0 bg-white font-mono font-medium text-gray-950 shadow outline-none ring-1 ring-gray-950/20 placeholder:text-gray-400 focus:border-accent focus:ring-gray-950"
+		class="focus:border-accent appearance-none border-0 bg-white font-mono font-medium text-gray-950 shadow outline-none ring-1 ring-gray-950/20 placeholder:text-gray-400 focus:ring-gray-950"
 		id="debug-log"
 		bind:value
 		on:change={(e) => {
 			// TODO: do zod validation
 			// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 			activeLog.set(JSON.parse(e.currentTarget.value));
+		}}
+	/>
+	<input
+		type="file"
+		accept=".json, .jsonl, .log, .txt"
+		bind:files
+		on:change={async (file) => {
+			const firstFile = file.currentTarget?.files?.item(0);
+			activeLog.set(firstFile ? await parseLogFile(firstFile) : { name: '', logs: [] });
 		}}
 	/>
 </div>
