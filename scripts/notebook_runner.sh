1ce2be4ec371b84884b03723ea7bfdc894bbce74
niklas <niklas.finken@aleph-alpha.com>
2023-11-06 17:44:21 +0100

    remove debug_log from qa notebook

--- scripts/notebook_runner.sh
+++ scripts/notebook_runner.sh
@@ -2,6 +2,6 @@
 # next line loads AA_TOKEN from .env file when running bash script locally. In CI this is not necessary since AA_TOKEN is environment variable.
 [ -f .env ] && source .env
 # Find all .ipynb files in the directory and pass them to xargs for parallel execution
-rm --force --recursive src/examples/*.nbconvert.ipynb src/examples/.ipynb_checkpoints
+rm -rf src/examples/*.nbconvert.ipynb src/examples/.ipynb_checkpoints
 find src/examples/ -name "*.ipynb" | xargs -I {} --max-procs 10 bash -c "AA_TOKEN=\"$AA_TOKEN\" poetry run jupyter nbconvert --to notebook --execute {}"
-rm --force src/examples/*.nbconvert.ipynb
+rm -f src/examples/*.nbconvert.ipynb
