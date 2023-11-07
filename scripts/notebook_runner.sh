4fe799307263d53c7f75cd1bb7ef6ce893d14836
Volker Stampa <Volker.Stampa@aleph-alpha.com>
2023-11-07 09:42:28 +0100

    Run only 3 notebook concurrently
    Running more seems to lead to resource problems on
    github ci

--- scripts/notebook_runner.sh
+++ scripts/notebook_runner.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env -S bash -eu -o pipefail
 # next line loads AA_TOKEN from .env file when running bash script locally. In CI this is not necessary since AA_TOKEN is environment variable.
 [ -f .env ] && source .env
+export AA_TOKEN
 # Find all .ipynb files in the directory and pass them to xargs for parallel execution
 rm -rf src/examples/*.nbconvert.ipynb src/examples/.ipynb_checkpoints
-find src/examples/ -name "*.ipynb" | while read nb; do poetry run jupyter nbconvert --to notebook --execute "$nb"; done
+find src/examples/ -name "*.ipynb" | xargs --max-args 1 --max-procs 3 poetry run jupyter nbconvert --to notebook --execute
 rm -f src/examples/*.nbconvert.ipynb
