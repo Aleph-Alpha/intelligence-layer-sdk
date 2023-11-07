fa41ccc45c3a62211781888467f5c888d85a1e1e
Volker Stampa <Volker.Stampa@aleph-alpha.com>
2023-11-07 09:29:46 +0100

    Run nb sequentially

--- scripts/notebook_runner.sh
+++ scripts/notebook_runner.sh
@@ -3,5 +3,5 @@
 [ -f .env ] && source .env
 # Find all .ipynb files in the directory and pass them to xargs for parallel execution
 rm -rf src/examples/*.nbconvert.ipynb src/examples/.ipynb_checkpoints
-find src/examples/ -name "*.ipynb" | xargs -I {} --max-procs 10 bash -c "AA_TOKEN=\"$AA_TOKEN\" poetry run jupyter nbconvert --to notebook --execute {}"
+find src/examples/ -name "*.ipynb" | while read nb; do poetry run jupyter nbconvert --to notebook --execute "$nb"; done
 rm -f src/examples/*.nbconvert.ipynb
