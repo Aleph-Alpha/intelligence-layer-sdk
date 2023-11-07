3dfa0313d8649ae9b6a349f4f422b1526cef7df1
Rutger Cappendijk <rutger.cappendijk@aleph-alpha.com>
2023-11-07 13:24:09 +0100

    fixed mypy error

--- tests/use_cases/summarize/test_keyword_extract.py
+++ tests/use_cases/summarize/test_keyword_extract.py
@@ -15,7 +15,7 @@ def keyword_extract(client: Client) -> KeywordExtract:
     return KeywordExtract(client)
 
 
-def test_keyword_extract_works(keyword_extract: KeywordExtract):
+def test_keyword_extract_works(keyword_extract: KeywordExtract) -> None:
     input = KeywordExtractInput(
         chunk=Chunk("text about computers"), language=Language("en")
     )
