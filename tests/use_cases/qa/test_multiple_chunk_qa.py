2c97457137e77ff4f6e8d648d23c82ff7da036f2
Volker Stampa <Volker.Stampa@aleph-alpha.com>
2023-11-09 13:03:50 +0100

    add ml-instructions for merging answers

--- tests/use_cases/qa/test_multiple_chunk_qa.py
+++ tests/use_cases/qa/test_multiple_chunk_qa.py
@@ -4,7 +4,8 @@ from aleph_alpha_client import Client
 from pytest import fixture
 
 from intelligence_layer.core.chunk import Chunk
-from intelligence_layer.core.tracer import InMemoryTracer, NoOpTracer
+from intelligence_layer.core.detect_language import Language
+from intelligence_layer.core.tracer import NoOpTracer
 from intelligence_layer.use_cases.qa.multiple_chunk_qa import (
     MultipleChunkQa,
     MultipleChunkQaInput,
@@ -16,61 +17,55 @@ def qa(client: Client) -> MultipleChunkQa:
     return MultipleChunkQa(client)
 
 
-def test_multiple_chunk_qa_with_answer(qa: MultipleChunkQa) -> None:
-    question = "What is the name of Paul Nicolas' brother?"
+CHUNK_CONTAINING_ANSWER = Chunk(
+    "Paul Nicolas lost his mother at the age of 3, and then his father in 1914.[3] He was raised by his mother-in-law together with his brother Henri. "
+    "He began his football career with Saint-Mandé Club in 1916. Initially, he played as a defender, but he quickly realized that his destiny laid at the "
+    "forefront since he scored many goals.[3] In addition to his goal-scoring instinct, Nicolas also stood out for his strong character on the pitch, "
+    "and these two qualities combined eventually drew the attention of Mr. Fort, the then president of the Gallia Club, who signed him as a centre-forward in 1916."
+)
+RELATED_CHUNK_WITHOUT_ANSWER = Chunk(
+    "In addition to his goal-scoring instinct, Nicolas also stood out for his strong character on the pitch, and these two qualities combined eventually drew the "
+    "attention of Mr. Fort, the then president of the Gallia Club, who signed him as a centre-forward in 1916. "
+)
+RELATED_QUESTION = "What is the name of Paul Nicolas' brother?"
+IMPORTANT_PART_OF_CORRECT_ANSWER = "Henri"
+UNRELATED_QUESTION = "What is the the capital of Germany?"
 
-    chunks: Sequence[Chunk] = [
-        Chunk(
-            "Paul Nicolas lost his mother at the age of 3, and then his father in 1914.[3] He was raised by his mother-in-law together with his brother Henri. He began his football career with Saint-Mandé Club in 1916. Initially, he played as a defender, but he quickly realized that his destiny laid at the forefront since he scored many goals.[3] In addition to his goal-scoring instinct, Nicolas also stood out for his strong character on the pitch, and these two qualities combined eventually drew the attention of Mr. Fort, the then president of the Gallia Club, who signed him as a centre-forward in 1916."
-        )
-    ]
 
-    input = MultipleChunkQaInput(chunks=chunks, question=question)
+def test_multiple_chunk_qa_with_mulitple_chunks(qa: MultipleChunkQa) -> None:
+    chunks: Sequence[Chunk] = [CHUNK_CONTAINING_ANSWER, RELATED_CHUNK_WITHOUT_ANSWER]
+
+    input = MultipleChunkQaInput(chunks=chunks, question=RELATED_QUESTION)
     output = qa.run(input, NoOpTracer())
 
     assert output.answer
-    assert "Henri" in output.answer
-    assert any(
-        any("Henri" in highlight for highlight in source.highlights)
-        for source in output.subanswers
-    )
+    assert IMPORTANT_PART_OF_CORRECT_ANSWER in output.answer
     assert len(output.subanswers) == 1
+    assert output.subanswers[0].chunk == chunks[0]
+    assert any(
+        IMPORTANT_PART_OF_CORRECT_ANSWER in highlight
+        for highlight in output.subanswers[0].highlights
+    )
 
 
 def test_multiple_chunk_qa_without_answer(qa: MultipleChunkQa) -> None:
-    question = "What is the the capital of Germany?"
+    chunks: Sequence[Chunk] = [CHUNK_CONTAINING_ANSWER]
 
-    chunks: Sequence[Chunk] = [
-        Chunk(
-            "Paul Nicolas lost his mother at the age of 3, and then his father in 1914.[3] He was raised by his mother-in-law together with his brother Henri. He began his football career with Saint-Mandé Club in 1916. Initially, he played as a defender, but he quickly realized that his destiny laid at the forefront since he scored many goals.[3] In addition to his goal-scoring instinct, Nicolas also stood out for his strong character on the pitch, and these two qualities combined eventually drew the attention of Mr. Fort, the then president of the Gallia Club, who signed him as a centre-forward in 1916."
-        )
-    ]
-
-    input = MultipleChunkQaInput(chunks=chunks, question=question)
-    tracer = InMemoryTracer()
-    output = qa.run(input, tracer)
+    input = MultipleChunkQaInput(chunks=chunks, question=UNRELATED_QUESTION)
+    output = qa.run(input, NoOpTracer())
 
     assert output.answer is None
 
 
-def test_multiple_chunk_qa_with_mulitple_chunks(qa: MultipleChunkQa) -> None:
-    question = "What is the name of Paul Nicolas' brother?"
+def test_multiple_chunk_qa_with_spanish_question(qa: MultipleChunkQa) -> None:
+    question = "¿Cómo se llama el hermano de Paul Nicola?"
+    chunks = [CHUNK_CONTAINING_ANSWER, CHUNK_CONTAINING_ANSWER]
 
-    chunks: Sequence[Chunk] = [
-        Chunk(
-            "Paul Nicolas lost his mother at the age of 3, and then his father in 1914.[3] He was raised by his mother-in-law together with his brother Henri. He began his football career with Saint-Mandé Club in 1916. Initially, he played as a defender, but he quickly realized that his destiny laid at the forefront since he scored many goals.[3]"
-        ),
-        Chunk(
-            "In addition to his goal-scoring instinct, Nicolas also stood out for his strong character on the pitch, and these two qualities combined eventually drew the attention of Mr. Fort, the then president of the Gallia Club, who signed him as a centre-forward in 1916. "
-        ),
-    ]
+    input = MultipleChunkQaInput(
+        chunks=chunks, question=question, language=Language("es")
+    )
+    output = qa.run(input, NoOpTracer())
 
-    input = MultipleChunkQaInput(chunks=chunks, question=question)
-    tracer = InMemoryTracer()
-    output = qa.run(input, tracer)
-
+    assert len(output.subanswers) == len(chunks)
     assert output.answer
-    assert "Henri" in output.answer
-    assert len(output.subanswers) == 1
-    assert output.subanswers[0].chunk == chunks[0]
-    assert any("Henri" in highlight for highlight in output.subanswers[0].highlights)
+    assert "hermano" in output.answer
