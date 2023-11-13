fb95a496ab4b8b7d612745e6dc30d690a4c215f7
niklas <niklas.finken@aleph-alpha.com>
2023-11-13 11:19:37 +0100

    no op SingleChunkSummarizeEvaluator

--- tests/use_cases/summarize/test_single_chunk_few_shot_summarize.py
+++ tests/use_cases/summarize/test_single_chunk_few_shot_summarize.py
@@ -1,6 +1,3 @@
-from aleph_alpha_client import Client
-from pytest import fixture
-
 from intelligence_layer.core.chunk import Chunk
 from intelligence_layer.core.detect_language import Language
 from intelligence_layer.core.tracer import NoOpTracer
@@ -10,33 +7,22 @@ from intelligence_layer.use_cases.summarize.single_chunk_few_shot_summarize impo
 from intelligence_layer.use_cases.summarize.summarize import SingleChunkSummarizeInput
 
 
-@fixture
-def single_chunk_few_shot_summarize(client: Client) -> SingleChunkFewShotSummarize:
-    return SingleChunkFewShotSummarize(client, "luminous-extended", 128)
-
-
 def test_high_compression_summarize_en(
-    single_chunk_few_shot_summarize: SingleChunkFewShotSummarize,
+    single_chunk_few_shot_summarize: SingleChunkFewShotSummarize, chunk: Chunk
 ) -> None:
-    text = Chunk(
-        "The brown bear (Ursus arctos) is a large bear species found across Eurasia and North America.[1][3] In North America, the populations of brown bears are called grizzly bears, while the subspecies that inhabits the Kodiak Islands of Alaska is known as the Kodiak bear. It is one of the largest living terrestrial members of the order Carnivora, rivaled in size only by its closest relative, the polar bear (Ursus maritimus), which is much less variable in size and slightly bigger on average.[4][5][6][7][8] The brown bear's range includes parts of Russia, Central Asia, the Himalayas, China, Canada, the United States, Hokkaido, Scandinavia, Finland, the Balkans, the Picos de Europa and the Carpathian region (especially Romania), Iran, Anatolia, and the Caucasus.[1][9] The brown bear is recognized as a national and state animal in several European countries.[10]"
-    )
-    input = SingleChunkSummarizeInput(chunk=text, language=Language("en"))
+    input = SingleChunkSummarizeInput(chunk=chunk, language=Language("en"))
     output = single_chunk_few_shot_summarize.run(input, NoOpTracer())
 
     assert output.summary
     assert "bear" in output.summary.lower()
-    assert len(output.summary) < len(text)
+    assert len(output.summary) < len(chunk)
 
 
 def test_high_compression_summarize_is_language_sensitive(
-    single_chunk_few_shot_summarize: SingleChunkFewShotSummarize,
+    single_chunk_few_shot_summarize: SingleChunkFewShotSummarize, chunk: Chunk
 ) -> None:
-    text = Chunk(
-        "The brown bear (Ursus arctos) is a large bear species found across Eurasia and North America.[1][3] In North America, the populations of brown bears are called grizzly bears, while the subspecies that inhabits the Kodiak Islands of Alaska is known as the Kodiak bear. It is one of the largest living terrestrial members of the order Carnivora, rivaled in size only by its closest relative, the polar bear (Ursus maritimus), which is much less variable in size and slightly bigger on average.[4][5][6][7][8] The brown bear's range includes parts of Russia, Central Asia, the Himalayas, China, Canada, the United States, Hokkaido, Scandinavia, Finland, the Balkans, the Picos de Europa and the Carpathian region (especially Romania), Iran, Anatolia, and the Caucasus.[1][9] The brown bear is recognized as a national and state animal in several European countries.[10]"
-    )
-    input_en = SingleChunkSummarizeInput(chunk=text, language=Language("en"))
-    input_de = SingleChunkSummarizeInput(chunk=text, language=Language("de"))
+    input_en = SingleChunkSummarizeInput(chunk=chunk, language=Language("en"))
+    input_de = SingleChunkSummarizeInput(chunk=chunk, language=Language("de"))
     output_en, output_de = single_chunk_few_shot_summarize.run_concurrently(
         [input_en, input_de], NoOpTracer(), concurrency_limit=2
     )
---
+++ tests/use_cases/summarize/test_summarize.py
@@ -0,0 +1,27 @@
+from pytest import fixture
+from intelligence_layer.core.chunk import Chunk
+from intelligence_layer.core.detect_language import Language
+from intelligence_layer.core.evaluator import Example, Dataset
+from intelligence_layer.core.tracer import NoOpTracer
+from intelligence_layer.use_cases.summarize.single_chunk_few_shot_summarize import SingleChunkFewShotSummarize
+from intelligence_layer.use_cases.summarize.summarize import SingleChunkSummarizeEvaluator, SingleChunkSummarizeInput
+
+
+@fixture
+def single_chunk_summarize_evaluator(single_chunk_few_shot_summarize: SingleChunkFewShotSummarize) -> SingleChunkSummarizeEvaluator:
+    return SingleChunkSummarizeEvaluator(single_chunk_few_shot_summarize)
+
+
+def test_single_chunk_summarize_evaluator(single_chunk_summarize_evaluator: SingleChunkSummarizeEvaluator, chunk: Chunk, no_op_tracer: NoOpTracer) -> None:
+    input = SingleChunkSummarizeInput(chunk=chunk, language=Language("en"))
+    bad_expected_output = "Heute ist das Wetter schön."
+    good_expected_output = "The brown bear is a large mammal that lives in Eurasia and North America."
+    outputs = [bad_expected_output, good_expected_output]
+    dataset = Dataset(name="summarize_eval_test", examples=[
+        Example(input=input, expected_output=output) for output in outputs
+    ])
+    aggregated_evaluation = single_chunk_summarize_evaluator.evaluate_dataset(dataset, no_op_tracer)
+
+    assert len(aggregated_evaluation.evaluations) == len(outputs)
+    assert aggregated_evaluation.evaluations[0].bleu < aggregated_evaluation.evaluations[1].bleu
+    assert aggregated_evaluation.evaluations[0].rouge < aggregated_evaluation.evaluations[1].rouge
