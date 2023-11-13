6efef965366171c16421c652cf17a10a3cea3cf8
Ben Brandt <benjamin.j.brandt@gmail.com>
2023-11-13 13:39:31 +0100

    Tweak analytics script

--- trace-viewer/playwright.config.ts
+++ trace-viewer/playwright.config.ts
@@ -3,7 +3,10 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 4173
+		port: 4173,
+		env: {
+			PUBLIC_DISABLE_ANALYTICS: 'true'
+		}
 	},
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/
--- trace-viewer/src/routes/+layout.svelte
+++ trace-viewer/src/routes/+layout.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
+	import { PUBLIC_DISABLE_ANALYTICS } from '$env/static/public';
 	import '../app.css';
 </script>
+
+<svelte:head>
+	{#if !PUBLIC_DISABLE_ANALYTICS || PUBLIC_DISABLE_ANALYTICS !== "false" }
+		<!-- Fathom - beautiful, simple website analytics -->
+		<script
+			src="https://cdn-eu.usefathom.com/script.js"
+			data-spa="auto"
+			data-site="GLRCMWLG"
+			defer
+		></script>
+		<!-- / Fathom -->
+	{/if}
+</svelte:head>
 
 <slot />
