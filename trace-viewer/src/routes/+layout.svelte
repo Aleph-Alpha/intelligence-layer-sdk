6283aa74ce9ea68ed188dcb79e6baa985779abe6
Ben Brandt <benjamin.j.brandt@gmail.com>
2023-11-13 13:48:01 +0100

    Fix for prettier bug

--- trace-viewer/src/routes/+layout.svelte
+++ trace-viewer/src/routes/+layout.svelte
@@ -5,15 +5,12 @@
 
 <svelte:head>
 	{#if !env.PUBLIC_DISABLE_ANALYTICS || env.PUBLIC_DISABLE_ANALYTICS !== 'false'}
-		<!-- Fathom - beautiful, simple website analytics -->
-		<!-- Fathom - beautiful, simple website analytics -->
 		<script
 			src="https://cdn-eu.usefathom.com/script.js"
 			data-spa="auto"
 			data-site="GLRCMWLG"
 			defer
 		></script>
-		<!-- / Fathom -->
 	{/if}
 </svelte:head>
 
