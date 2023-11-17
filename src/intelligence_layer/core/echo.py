7340193f0737c5aac21058ab6a5cffe564e36adf
Rutger Cappendijk <rutger.cappendijk@aleph-alpha.com>
2023-11-17 13:58:57 +0100

    reproduce ci error

--- src/intelligence_layer/core/echo.py
+++ src/intelligence_layer/core/echo.py
@@ -107,6 +107,10 @@ class EchoTask(Task[EchoInput, EchoOutput]):
         for token, log_prob in zip(
             expected_completion_tokens, log_prob_dicts, strict=True
         ):
+            if not token.token in log_prob:
+                print(f"token: {token}")
+                print(f"Log prob: {log_prob}")
+
             assert token.token in log_prob
             tokens_with_prob.append(
                 TokenWithLogProb(
