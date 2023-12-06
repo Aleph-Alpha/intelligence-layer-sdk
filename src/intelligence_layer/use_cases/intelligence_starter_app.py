bfa155ec9a1d9a4aa1ec6d9fcf264dc50a25bf4d
Rutger Cappendijk <rutger.cappendijk@aleph-alpha.com>
2023-12-06 09:24:20 +0100

    changed client to limited concurrency client

--- src/intelligence_layer/use_cases/intelligence_starter_app.py
+++ src/intelligence_layer/use_cases/intelligence_starter_app.py
@@ -3,8 +3,11 @@ import os
 from aleph_alpha_client import Client
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from intelligence_layer.connectors.limited_concurrency_client import LimitedConcurrencyClient
 
+from intelligence_layer.connectors import (
+    AlephAlphaClientProtocol,
+    LimitedConcurrencyClient,
+)
 from intelligence_layer.core import IntelligenceApp
 from intelligence_layer.use_cases.classify.prompt_based_classify import (
     PromptBasedClassify,
@@ -16,7 +19,7 @@ from intelligence_layer.use_cases.summarize.long_context_high_compression_summar
 
 
 class IntelligenceStarterApp(IntelligenceApp):
-    def __init__(self, fast_api_app: FastAPI, client: Client) -> None:
+    def __init__(self, fast_api_app: FastAPI, client: AlephAlphaClientProtocol) -> None:
         super().__init__(fast_api_app)
         prompt_based_classify = PromptBasedClassify(client)
         self.register_task(prompt_based_classify, "/classify")
@@ -26,12 +29,12 @@ class IntelligenceStarterApp(IntelligenceApp):
         self.register_task(summarize, "/summarize")
 
 
-
 def main() -> None:
     load_dotenv()
     aa_token = os.getenv("AA_TOKEN")
     assert aa_token
-    client = LimitedConcurrencyClient(aa_token)
+    aa_client = Client(aa_token)
+    client = LimitedConcurrencyClient(aa_client)
     fast_api = FastAPI()
     app = IntelligenceStarterApp(fast_api, client)
     app.serve()
