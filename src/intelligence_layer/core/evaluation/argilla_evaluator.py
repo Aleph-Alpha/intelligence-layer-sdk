a048cb8a862def2bab125b3e0822a7c2ef50c17c
niklas <niklas.finken@aleph-alpha.com>
2023-12-07 12:42:03 +0100

    prelim

--- src/intelligence_layer/core/evaluation/argilla_evaluator.py
+++ src/intelligence_layer/core/evaluation/argilla_evaluator.py
@@ -1,7 +1,9 @@
 import os
 import re
+from typing import Any, Iterable, Mapping, Optional, Sequence
+
 import requests
-from typing import Iterable, Optional, Sequence
+from pydantic import BaseModel, validator
 
 from intelligence_layer.core.evaluation.domain import (
     AggregatedEvaluation,
@@ -18,6 +20,27 @@ from intelligence_layer.core.task import Input, Output, Task
 from intelligence_layer.core.tracer import Tracer
 
 
+class Field(BaseModel):
+    name: str
+    path: str
+
+    @validator("path")
+    def validate_path(cls, v: str) -> str:
+        if not isinstance(v, str) or "." not in v:
+            raise ValueError("Path must be a string with dot-separated values")
+        return v
+
+    def get_value(self, data_dict: dict) -> Any:
+        keys = self.path.split(".")
+        value = data_dict
+        for key in keys:
+            if key in value:
+                value = value[key]
+            else:
+                raise KeyError(f"Key '{key}' not found in the provided dictionary.")
+        return value
+
+
 class ArgillaClient:
     def __init__(
         self, api_url: Optional[str] = None, api_key: Optional[str] = None
@@ -27,36 +50,65 @@ class ArgillaClient:
         self.headers = {
             "accept": "application/json",
             "X-Argilla-Api-Key": self.api_key,
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         }
 
-    def _create_dataset(self, name: str, workspace_id: str, guidelines: str = "No guidelines.") -> str:
+    def upload(
+        self,
+        fields: Sequence[Field],
+        examples: Sequence[tuple[Example[Input, ExpectedOutput], Output]],
+    ) -> None:
+        name = "name_test_dataset"
+        workspace_id = "15657307-9780-44e5-87ba-4ad024a49a88"
+
+        datasets = self._list_datasets(workspace_id)
+        dataset_id = next(
+            (item["id"] for item in datasets["items"] if item["name"] == name), None
+        )
+        if not dataset_id:
+            dataset_id = self._create_dataset(name, workspace_id)["id"]
+        field_names = [field.name for field in fields]
+
+    def _list_datasets(self, workspace_id: str) -> Sequence[Any]:
+        url = self.api_url + f"api/v1/me/datasets?workspace_id={workspace_id}"
+        response = requests.get(url, headers=self.headers)
+        response.raise_for_status()
+        return response.json()
+
+    def _create_dataset(
+        self, name: str, workspace_id: str, guidelines: str = "No guidelines."
+    ) -> Mapping[str, Any]:
         url = self.api_url + "api/v1/datasets"
         data = {
             "name": name,
             "guidelines": guidelines,
             "workspace_id": workspace_id,
-            "allow_extra_metadata": True
+            "allow_extra_metadata": True,
         }
         response = requests.post(url, json=data, headers=self.headers)
-        if response.status_code // 100 == 2:
-            return response.json()["id"]
-        elif response.status_code == 409:
-            detail = response.json()["detail"]
-            pattern = r'`([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})`'
-            return re.search(pattern, detail).group(1)
         response.raise_for_status()
+        return response.json()
 
+    def _list_fields(self, dataset_id: str) -> Sequence[Any]:
+        url = self.api_url + f"api/v1/datasets/{dataset_id}/fields"
+        response = requests.get(url, headers=self.headers)
+        response.raise_for_status()
+        return response.json()
 
-    def upload(
-        self, examples: Sequence[tuple[Example[Input, ExpectedOutput], Output]]
-    ) -> None:
-        name = "name_test_dataset"
-        workspace_id = "15657307-9780-44e5-87ba-4ad024a49a88"
-        dataset_id = self._create_dataset(name, workspace_id)
-        fields = [
-            
-        ]
+    def _create_field(
+        self, name: str, title: str, dataset_id: str
+    ) -> Mapping[str, Any]:
+        url = self.api_url + f"api/v1/datasets/{dataset_id}/fields"
+        data = {
+            "name": name,
+            "title": title,
+            "required": True,
+            "settings": {"type": "text", "use_markdown": False},
+        }
+        response = requests.post(url, json=data, headers=self.headers)
+        response.raise_for_status()
+        return response.json()
+
 
 class ArgillaEvaluator(
     BaseEvaluator[Input, Output, ExpectedOutput, Evaluation, AggregatedEvaluation]
