67992ef4d50bfe35c90b8b5eb95bfa96f8082258
Rutger Cappendijk <rutger.cappendijk@aleph-alpha.com>
2023-12-12 17:14:21 +0100

    linting

--- src/intelligence_layer/core/evaluation/dataset_repository.py
+++ src/intelligence_layer/core/evaluation/dataset_repository.py
@@ -1,20 +1,33 @@
 from abc import ABC, abstractmethod
 from typing import Iterable, Optional, Sequence, cast
 from uuid import uuid4
-from intelligence_layer.core.evaluation.domain import Example, ExpectedOutput, SequenceDataset, Dataset
+
+from intelligence_layer.core.evaluation.domain import (
+    Dataset,
+    Example,
+    ExpectedOutput,
+    SequenceDataset,
+)
 from intelligence_layer.core.task import Input
-
-
 from intelligence_layer.core.tracer import PydanticSerializable
 
 
 class DatasetRepository(ABC):
     @abstractmethod
-    def create_dataset(self, name: str, examples: Iterable[tuple[PydanticSerializable, PydanticSerializable]]) -> str:
+    def create_dataset(
+        self,
+        name: str,
+        examples: Iterable[tuple[PydanticSerializable, PydanticSerializable]],
+    ) -> str:
         ...
 
     @abstractmethod
-    def dataset(self, id: str, input_type: type[Input], expected_output_type: type[ExpectedOutput]) -> Optional[Dataset[Input, ExpectedOutput]]:
+    def dataset(
+        self,
+        id: str,
+        input_type: type[Input],
+        expected_output_type: type[ExpectedOutput],
+    ) -> Optional[Dataset[Input, ExpectedOutput]]:
         ...
 
     @abstractmethod
@@ -24,21 +37,36 @@ class DatasetRepository(ABC):
     @abstractmethod
     def list_datasets(self) -> Sequence[tuple[str, str]]:
         ...
-    
 
 
 class InMemoryDatasetRepository(DatasetRepository):
-    """A repository to store datasets for evaluation.
-    """
+    """A repository to store datasets for evaluation."""
+
     _datasets: dict[str, Dataset[PydanticSerializable, PydanticSerializable]] = {}
-    def create_dataset(self, name: str, examples: Iterable[tuple[PydanticSerializable, PydanticSerializable]]) -> str:
-        in_memory_examples = [cast(Example[PydanticSerializable, PydanticSerializable],Example(input=example_input, expected_output=example_output)) for (example_input, example_output) in examples]
+
+    def create_dataset(
+        self,
+        name: str,
+        examples: Iterable[tuple[PydanticSerializable, PydanticSerializable]],
+    ) -> str:
+        in_memory_examples = [
+            cast(
+                Example[PydanticSerializable, PydanticSerializable],
+                Example(input=example_input, expected_output=example_output),
+            )
+            for (example_input, example_output) in examples
+        ]
         id = str(uuid4())
         self._datasets[id] = SequenceDataset(name=name, examples=in_memory_examples)
         return id
-    
-    def dataset(self, id: str, input_type: type[Input], expected_output_type: Optional[type[ExpectedOutput]]=None) -> Optional[Dataset[Input, ExpectedOutput]]:
-        return cast(Dataset[Input, ExpectedOutput],self._datasets.get(id))
+
+    def dataset(
+        self,
+        id: str,
+        input_type: type[Input],
+        expected_output_type: Optional[type[ExpectedOutput]] = None,
+    ) -> Optional[Dataset[Input, ExpectedOutput]]:
+        return cast(Dataset[Input, ExpectedOutput], self._datasets.get(id))
 
     def delete_dataset(self, id: str) -> None:
         self._datasets.pop(id, None)
--- tests/core/evaluation/test_dataset_repository.py
+++ tests/core/evaluation/test_dataset_repository.py
@@ -1,5 +1,6 @@
 from pytest import fixture
-from intelligence_layer.core import InMemoryDatasetRepository, Example
+
+from intelligence_layer.core import Example, InMemoryDatasetRepository
 from tests.conftest import DummyStringInput, DummyStringOutput
 
 
@@ -9,39 +10,63 @@ def in_memory_dataset_repository() -> InMemoryDatasetRepository:
 
 
 def test_in_memory_dataset_repository_can_store_dataset(
-        in_memory_dataset_repository: InMemoryDatasetRepository,
-        dummy_string_example: Example[DummyStringInput, DummyStringOutput]
-    ) -> None:
-    id = in_memory_dataset_repository.create_dataset(name="dummy dataset", examples=[(dummy_string_example.input, dummy_string_example.expected_output)])
+    in_memory_dataset_repository: InMemoryDatasetRepository,
+    dummy_string_example: Example[DummyStringInput, DummyStringOutput],
+) -> None:
+    id = in_memory_dataset_repository.create_dataset(
+        name="dummy dataset",
+        examples=[(dummy_string_example.input, dummy_string_example.expected_output)],
+    )
     assert id in in_memory_dataset_repository._datasets
-    dataset = in_memory_dataset_repository.dataset(id, input_type=DummyStringInput, expected_output_type=DummyStringOutput)
+    dataset = in_memory_dataset_repository.dataset(
+        id, input_type=DummyStringInput, expected_output_type=DummyStringOutput
+    )
     assert dataset
     assert list(dataset.examples)[0].input == dummy_string_example.input
-    assert list(dataset.examples)[0].expected_output == dummy_string_example.expected_output
+    assert (
+        list(dataset.examples)[0].expected_output
+        == dummy_string_example.expected_output
+    )
+
 
 def test_in_memory_dataset_repository_returns_none(
-        in_memory_dataset_repository: InMemoryDatasetRepository,
+    in_memory_dataset_repository: InMemoryDatasetRepository,
 ) -> None:
     assert in_memory_dataset_repository.dataset("", DummyStringInput, None) is None
 
+
 def test_in_memory_dataset_repository_can_delete_dataset(
-        in_memory_dataset_repository: InMemoryDatasetRepository,
-        dummy_string_example: Example[DummyStringInput, DummyStringOutput]
+    in_memory_dataset_repository: InMemoryDatasetRepository,
+    dummy_string_example: Example[DummyStringInput, DummyStringOutput],
 ) -> None:
-    id = in_memory_dataset_repository.create_dataset(name="dummy dataset", examples=[(dummy_string_example.input, dummy_string_example.expected_output)])
+    id = in_memory_dataset_repository.create_dataset(
+        name="dummy dataset",
+        examples=[(dummy_string_example.input, dummy_string_example.expected_output)],
+    )
     assert in_memory_dataset_repository.dataset(id, DummyStringInput, DummyStringOutput)
     in_memory_dataset_repository.delete_dataset(id)
-    assert in_memory_dataset_repository.dataset(id, DummyStringInput, DummyStringOutput) is None
-    in_memory_dataset_repository.delete_dataset(id) #tests wether function is idempotent
-    
+    assert (
+        in_memory_dataset_repository.dataset(id, DummyStringInput, DummyStringOutput)
+        is None
+    )
+    in_memory_dataset_repository.delete_dataset(
+        id
+    )  # tests whether function is idempotent
+
 
 def test_in_memory_dataset_repository_can_list_datasets(
-        in_memory_dataset_repository: InMemoryDatasetRepository,
-        dummy_string_example: Example[DummyStringInput, DummyStringOutput]
+    in_memory_dataset_repository: InMemoryDatasetRepository,
+    dummy_string_example: Example[DummyStringInput, DummyStringOutput],
 ) -> None:
     dataset_name = "dummy_dataset"
-    id0 = in_memory_dataset_repository.create_dataset(name=dataset_name, examples=[(dummy_string_example.input, dummy_string_example.expected_output)])
-    id1 = in_memory_dataset_repository.create_dataset(name=dataset_name, examples=[(dummy_string_example.input, dummy_string_example.expected_output)])
+    id0 = in_memory_dataset_repository.create_dataset(
+        name=dataset_name,
+        examples=[(dummy_string_example.input, dummy_string_example.expected_output)],
+    )
+    id1 = in_memory_dataset_repository.create_dataset(
+        name=dataset_name,
+        examples=[(dummy_string_example.input, dummy_string_example.expected_output)],
+    )
     names_and_ids = in_memory_dataset_repository.list_datasets()
-    assert names_and_ids[0] == (dataset_name,id0)
-    assert names_and_ids[1] == (dataset_name,id1)
+    assert names_and_ids[0] == (dataset_name, id0)
+    assert names_and_ids[1] == (dataset_name, id1)
