ea112855d630785aaa9eb9976fc16e1321ac8a8a
niklas <niklas.finken@aleph-alpha.com>
2023-12-28 10:37:17 +0100

    Include second HF test in first

--- tests/core/evaluation/test_hugging_face.py
+++ tests/core/evaluation/test_hugging_face.py
@@ -22,8 +22,13 @@ def hf_repository(hf_token: str) -> HuggingFaceDatasetRepository:
 
 
 @fixture
-def example() -> Example[str, str]:
+def example1() -> Example[str, str]:
     return Example(input="hey", expected_output="ho", id="0")
+
+
+@fixture
+def example2() -> Example[str, str]:
+    return Example(input="ho", expected_output="hey", id="1")
 
 
 def test_hf_database_non_existing(hf_repository: HuggingFaceDatasetRepository) -> None:
@@ -37,30 +42,18 @@ def test_hf_database_non_existing(hf_repository: HuggingFaceDatasetRepository) -
 
 
 def test_hf_database_operations(
-    hf_repository: HuggingFaceDatasetRepository, example: Example[str, str]
+    hf_repository: HuggingFaceDatasetRepository, example1: Example[str, str], example2: Example[str, str]
 ) -> None:
-    dataset_id = hf_repository.create_dataset([example])
+    original_examples = [example1, example2]
+    dataset_id = hf_repository.create_dataset(original_examples)
     try:
         assert dataset_id in list(hf_repository.list_datasets())
         examples = hf_repository.examples_by_id(dataset_id, str, str)
         assert examples is not None
-        assert [e for e in examples] == [example]
-        assert hf_repository.example(dataset_id, example.id, str, str) == example
+        assert list(examples) == original_examples
+        assert hf_repository.example(dataset_id, example1.id, str, str) == example1
         assert hf_repository.example(dataset_id, "abc", str, str) is None
         hf_repository.delete_dataset(dataset_id)
         assert hf_repository.examples_by_id(dataset_id, str, str) is None
-    finally:
-        hf_repository.delete_dataset(dataset_id)
-
-
-def test_hf_database_stores_multiple_examples(
-    hf_repository: HuggingFaceDatasetRepository, example: Example[str, str]
-) -> None:
-    second_example = Example(input="ho", expected_output="hey", id="1")
-    dataset_id = hf_repository.create_dataset([example, second_example])
-    try:
-        examples = hf_repository.examples_by_id(dataset_id, str, str)
-        assert examples is not None
-        assert [e for e in examples] == [example, second_example]
     finally:
         hf_repository.delete_dataset(dataset_id)
