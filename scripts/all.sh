0e5ce4b4fa7fba63b373eb5e07e42889ce69312b
Niklas Koehnecke <niklas.koehnecke.ext@aleph-alpha.com>
2024-01-05 12:35:54 +0100

    Fix loading the .env in all.sh

--- scripts/all.sh
+++ scripts/all.sh
@@ -4,6 +4,11 @@ ProjectRoot="$(cd $(dirname "$0")/.. && pwd -P)"
 
 cd "$ProjectRoot"
 
+# see https://stackoverflow.com/questions/43267413/how-to-set-environment-variables-from-env-file
+set -a # automatically export all variables
+source .env
+set +a
+
 ./scripts/load_env.sh
 ./scripts/lint.sh
 ./scripts/doctest.sh
