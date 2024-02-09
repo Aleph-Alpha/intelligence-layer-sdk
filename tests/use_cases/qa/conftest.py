451b1dd6d92ffc9639289ae442a4f842aa7f2c66
Niklas Koehnecke <niklas.koehnecke.ext@aleph-alpha.com>
2024-02-09 15:43:21 +0100

    fix: lint

--- tests/use_cases/qa/conftest.py
+++ tests/use_cases/qa/conftest.py
@@ -1,10 +1,9 @@
+from pytest import fixture
+
 from intelligence_layer.connectors.limited_concurrency_client import (
     AlephAlphaClientProtocol,
 )
 from intelligence_layer.use_cases.qa.single_chunk_qa import SingleChunkQa
-
-
-from pytest import fixture
 
 
 @fixture
