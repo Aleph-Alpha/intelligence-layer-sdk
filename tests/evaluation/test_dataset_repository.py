ee57618d92bff4e3631d1cab32548592c2549cc3
Valentina Galata <valentina.galata@tngtech.com>
2024-03-05 11:21:24 +0100

    refactor(repository): return sorted outputs in data-repository methods
    Task: IL-301

--- tests/evaluation/test_dataset_repository.py
+++ tests/evaluation/test_dataset_repository.py
@@ -26,20 +26,22 @@ test_repository_fixtures = [
     "repository_fixture",
     test_repository_fixtures,
 )
-def test_file_dataset_repository_can_store_dataset(
+def test_dataset_repository_can_create_and_store_a_dataset(
     repository_fixture: str,
     dummy_string_example: Example[DummyStringInput, DummyStringOutput],
     request: FixtureRequest,
 ) -> None:
     dataset_repository: DatasetRepository = request.getfixturevalue(repository_fixture)
     dataset_id = dataset_repository.create_dataset(examples=[dummy_string_example])
-    examples = dataset_repository.examples(
-        dataset_id,
-        input_type=DummyStringInput,
-        expected_output_type=DummyStringOutput,
+    examples = list(
+        dataset_repository.examples(
+            dataset_id,
+            input_type=DummyStringInput,
+            expected_output_type=DummyStringOutput,
+        )
     )
-    assert examples is not None
-    examples = list(examples)
+
+    assert len(examples) == 1
     assert examples[0].input == dummy_string_example.input
     assert examples[0].expected_output == dummy_string_example.expected_output
 
@@ -48,21 +50,99 @@ def test_file_dataset_repository_can_store_dataset(
     "repository_fixture",
     test_repository_fixtures,
 )
-def test_file_dataset_repository_returns_empty_list_for_not_existing_dataset_id(
-    repository_fixture: str, request: FixtureRequest
+def test_examples_returns_all_examples_sorted_by_their_id(
+    repository_fixture: str,
+    request: FixtureRequest,
 ) -> None:
     dataset_repository: DatasetRepository = request.getfixturevalue(repository_fixture)
+    examples = [
+        Example(
+            input=DummyStringInput.any(),
+            expected_output=DummyStringOutput.any(),
+        )
+        for i in range(0, 10)
+    ]
+    dataset_id = dataset_repository.create_dataset(examples=examples)
 
-    examples = dataset_repository.examples("some_name", DummyStringInput, type(None))
+    stored_examples = list(
+        dataset_repository.examples(dataset_id, DummyStringInput, DummyStringOutput)
+    )
 
-    assert examples == []
+    assert stored_examples == sorted(examples, key=lambda example: example.id)
 
 
 @mark.parametrize(
     "repository_fixture",
     test_repository_fixtures,
 )
-def test_file_dataset_repository_can_delete_dataset(
+def test_examples_returns_an_empty_list_for_not_existing_dataset_id(
+    repository_fixture: str, request: FixtureRequest
+) -> None:
+    dataset_repository: DatasetRepository = request.getfixturevalue(repository_fixture)
+
+    examples = list(
+        dataset_repository.examples(
+            "not_existing_dataset_id", DummyStringInput, type(None)
+        )
+    )
+
+    assert len(examples) == 0
+
+
+@mark.parametrize(
+    "repository_fixture",
+    test_repository_fixtures,
+)
+def test_example_returns_example_for_existing_dataset_id_and_example_id(
+    repository_fixture: str,
+    dummy_string_example: Example[DummyStringInput, DummyStringOutput],
+    request: FixtureRequest,
+) -> None:
+    dataset_repository: DatasetRepository = request.getfixturevalue(repository_fixture)
+    dataset_id = dataset_repository.create_dataset(examples=[dummy_string_example])
+
+    example = dataset_repository.example(
+        dataset_id, dummy_string_example.id, DummyStringInput, DummyStringOutput
+    )
+
+    assert example == dummy_string_example
+
+
+@mark.parametrize("repository_fixture", test_repository_fixtures)
+def test_example_returns_none_for_not_existing_dataset_id_or_example_id(
+    repository_fixture: str,
+    dummy_string_example: Example[DummyStringInput, DummyStringOutput],
+    request: FixtureRequest,
+) -> None:
+    dataset_repository: DatasetRepository = request.getfixturevalue(repository_fixture)
+    dataset_id = dataset_repository.create_dataset(examples=[dummy_string_example])
+
+    examples = [
+        dataset_repository.example(
+            "not_existing_dataset_id",
+            dummy_string_example.id,
+            DummyStringInput,
+            DummyStringOutput,
+        ),
+        dataset_repository.example(
+            dataset_id, "not_existing_example_id", DummyStringInput, DummyStringOutput
+        ),
+        dataset_repository.example(
+            "not_existing_dataset_id",
+            "not_existing_example_id",
+            DummyStringInput,
+            DummyStringOutput,
+        ),
+    ]
+
+    assert examples == [None, None, None]
+
+
+@mark.parametrize(
+    "repository_fixture",
+    test_repository_fixtures,
+)
+def test_delete_dataset_deletes_a_dataset(
     repository_fixture: str,
     dummy_string_examples: Iterable[Example[DummyStringInput, DummyStringOutput]],
     request: FixtureRequest,
@@ -72,10 +152,13 @@ def test_file_dataset_repository_can_delete_dataset(
 
     dataset_repository.delete_dataset(dataset_id)
 
-    examples = dataset_repository.examples(
-        dataset_id, DummyStringInput, DummyStringOutput
+    dataset_ids = dataset_repository.dataset_ids()
+    examples = list(
+        dataset_repository.examples(dataset_id, DummyStringInput, DummyStringOutput)
     )
-    assert examples == []
+
+    assert dataset_id not in dataset_ids
+    assert len(examples) == 0
 
     dataset_repository.delete_dataset(
         dataset_id
@@ -86,14 +169,25 @@ def test_file_dataset_repository_can_delete_dataset(
     "repository_fixture",
     test_repository_fixtures,
 )
-def test_file_dataset_repository_can_list_datasets(
+def test_dataset_ids_returns_all_sorted_ids(
     repository_fixture: str,
-    dummy_string_example: Example[DummyStringInput, DummyStringOutput],
     request: FixtureRequest,
 ) -> None:
     dataset_repository: DatasetRepository = request.getfixturevalue(repository_fixture)
-    examples = [dummy_string_example]
-    dataset_id_1 = dataset_repository.create_dataset(examples=examples)
-    dataset_id_2 = dataset_repository.create_dataset(examples=examples)
-    dataset_names = dataset_repository.dataset_ids()
-    assert sorted(dataset_names) == sorted([dataset_id_1, dataset_id_2])
+    dataset_ids = []
+
+    for i in range(0, 10):
+        dataset_ids.append(
+            dataset_repository.create_dataset(
+                examples=[
+                    Example(
+                        input=DummyStringInput.any(),
+                        expected_output=DummyStringOutput.any(),
+                    )
+                ]
+            )
+        )
+
+    saved_dataset_ids = dataset_repository.dataset_ids()
+
+    assert saved_dataset_ids == sorted(dataset_ids)
