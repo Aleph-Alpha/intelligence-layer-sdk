a36a7f62ca1d38b992924fdae9b401dda890475f
niklas.finken <niklas.finken@aleph-alpha.com>
2024-03-08 12:20:14 +0100

    linting

--- tests/use_cases/classify/test_prompt_based_classify_with_definitions.py
+++ tests/use_cases/classify/test_prompt_based_classify_with_definitions.py
@@ -2,12 +2,12 @@ from typing import Sequence
 
 from pytest import fixture
 
-from intelligence_layer.core import NoOpTracer, TextChunk, LuminousControlModel
+from intelligence_layer.core import LuminousControlModel, NoOpTracer, TextChunk
 from intelligence_layer.use_cases import (
     ClassifyInput,
-    PromptBasedClassifyWithDefinitions,
     LabelWithDefinition,
-    SingleLabelClassifyOutput
+    PromptBasedClassifyWithDefinitions,
+    SingleLabelClassifyOutput,
 )
 
 
@@ -15,41 +15,37 @@ from intelligence_layer.use_cases import (
 def labels_with_definitions() -> Sequence[LabelWithDefinition]:
     return [
         LabelWithDefinition(
-            name="Dinosaur",
-            definition="Any text that is about dinosaurs."
+            name="Dinosaur", definition="Any text that is about dinosaurs."
         ),
+        LabelWithDefinition(name="Plant", definition="Any text that is about plants."),
         LabelWithDefinition(
-            name="Plant",
-            definition="Any text that is about plants."
+            name="Toy", definition="Everything that has something to do with toys."
         ),
-        LabelWithDefinition(
-            name="Toy",
-            definition="Everything that has something to do with toys."
-        )
     ]
 
 
 @fixture
 def prompt_based_classify_with_definitions(
     luminous_control_model: LuminousControlModel,
-    labels_with_definitions: Sequence[LabelWithDefinition]
+    labels_with_definitions: Sequence[LabelWithDefinition],
 ) -> PromptBasedClassifyWithDefinitions:
     return PromptBasedClassifyWithDefinitions(
-        labels_with_definitions,
-        luminous_control_model
+        labels_with_definitions, luminous_control_model
     )
 
 
 def test_prompt_based_classify_with_definitions_returns_score_for_all_labels(
     prompt_based_classify_with_definitions: PromptBasedClassifyWithDefinitions,
-    labels_with_definitions: Sequence[LabelWithDefinition]
+    labels_with_definitions: Sequence[LabelWithDefinition],
 ) -> None:
     classify_input = ClassifyInput(
         chunk=TextChunk("I love my cactus!"),
         labels=frozenset(label.name for label in labels_with_definitions),
     )
 
-    classify_output = prompt_based_classify_with_definitions.run(classify_input, NoOpTracer())
+    classify_output = prompt_based_classify_with_definitions.run(
+        classify_input, NoOpTracer()
+    )
 
     # Output contains everything we expect
     assert isinstance(classify_output, SingleLabelClassifyOutput)
