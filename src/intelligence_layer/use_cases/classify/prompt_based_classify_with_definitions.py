07559c219cf9978398428583cda3486842550c7b
niklas.finken <niklas.finken@aleph-alpha.com>
2024-03-08 12:20:14 +0100

    linting

--- src/intelligence_layer/use_cases/classify/prompt_based_classify_with_definitions.py
+++ src/intelligence_layer/use_cases/classify/prompt_based_classify_with_definitions.py
@@ -1,8 +1,19 @@
 from math import exp
 from typing import Sequence
+
 from aleph_alpha_client import Prompt
 from pydantic import BaseModel
-from intelligence_layer.core import CompleteInput, CompleteOutput, ControlModel, LuminousControlModel, Task, TaskSpan, TextChunk
+
+from intelligence_layer.core import (
+    CompleteInput,
+    CompleteOutput,
+    ControlModel,
+    LuminousControlModel,
+    Task,
+    TaskSpan,
+    TextChunk,
+)
+
 from .classify import ClassifyInput, Probability, SingleLabelClassifyOutput
 
 
@@ -21,7 +32,9 @@ class LabelWithDefinition(BaseModel):
         return f"{self.name}: {self.definition}"
 
 
-class PromptBasedClassifyWithDefinitions(Task[ClassifyInput, SingleLabelClassifyOutput]):
+class PromptBasedClassifyWithDefinitions(
+    Task[ClassifyInput, SingleLabelClassifyOutput]
+):
 
     INSTRUCTION: str = """Identify a class that describes the text adequately.
 Reply with only the class label."""
@@ -37,22 +50,26 @@ Reply with only the class label."""
         self._model = model or LuminousControlModel("luminous-base-control")
         self._instruction = instruction
 
-    def do_run(self, input: ClassifyInput, task_span: TaskSpan) -> SingleLabelClassifyOutput:
+    def do_run(
+        self, input: ClassifyInput, task_span: TaskSpan
+    ) -> SingleLabelClassifyOutput:
         complete_output = self._model.complete(
             CompleteInput(
                 prompt=self._get_prompt(input.chunk, input.labels),
                 completion_bias_inclusion=list(input.labels),
-                log_probs=len(input.labels) * 2
+                log_probs=len(input.labels) * 2,
             ),
-            task_span
+            task_span,
         )
-        return SingleLabelClassifyOutput(
-            scores=self._build_scores(complete_output)
-        )
+        return SingleLabelClassifyOutput(scores=self._build_scores(complete_output))
 
     def _get_prompt(self, chunk: TextChunk, labels: frozenset[str]) -> Prompt:
         def format_input(text: str, labels: frozenset[str]) -> str:
-            definitions = '\n'.join(label.to_string() for label in self._labels_with_definitions if label.name in labels)
+            definitions = "\n".join(
+                label.to_string()
+                for label in self._labels_with_definitions
+                if label.name in labels
+            )
             return f"""Text: {text}
 
 Labels:
@@ -61,13 +78,15 @@ Labels:
 Definitions:
 {definitions}"""
 
-        unexpected_labels = labels - set(label.name for label in self._labels_with_definitions)
+        unexpected_labels = labels - set(
+            label.name for label in self._labels_with_definitions
+        )
         if unexpected_labels:
             raise ValueError(f"Got unexpected labels: {', '.join(unexpected_labels)}")
 
         return self._model.to_instruct_prompt(
             instruction=self._instruction,
-            input=format_input(text=str(chunk), labels=labels)
+            input=format_input(text=str(chunk), labels=labels),
         )
 
     def _build_scores(self, complete_output: CompleteOutput) -> dict[str, Probability]:
