64ce8736dd0b2af28e6dfd45921eb88702fe288a
philipp <philipp.siedler@aleph-alpha.com>
2024-03-13 14:19:08 +0000

    Add SingleHuggingFaceDatasetRepository
    For integration with EleutherAI Harness Datasets

---
+++ src/intelligence_layer/evaluation/infrastructure/single_huggingface_dataset_repository.py
@@ -0,0 +1,80 @@
+from typing import Iterable, Sequence, cast
+
+from datasets import Dataset as HFDataset  # type: ignore
+from datasets import DatasetDict, IterableDataset, IterableDatasetDict
+from pydantic import BaseModel
+
+from intelligence_layer.core.task import Input
+from intelligence_layer.evaluation.data_storage.dataset_repository import (
+    DatasetRepository,
+)
+from intelligence_layer.evaluation.domain import Dataset, Example, ExpectedOutput
+
+
+class MultipleChoiceInput(BaseModel):
+    question: str
+    choices: Sequence[str]
+
+
+class SingleHuggingfaceDatasetRepository(DatasetRepository):
+    def __init__(
+        self,
+        huggingface_dataset: (
+            DatasetDict | HFDataset | IterableDatasetDict | IterableDataset
+        ),
+    ) -> None:
+        self._huggingface_dataset = huggingface_dataset
+
+    def create_dataset(
+        self, examples: Iterable[Example[Input, ExpectedOutput]], dataset_name: str
+    ) -> Dataset:
+        raise NotImplementedError
+
+    def dataset(self, dataset_id: str) -> Dataset | None:
+        raise NotImplementedError
+
+    def dataset_ids(self) -> Iterable[str]:
+        raise NotImplementedError
+
+    def delete_dataset(self, dataset_id: str) -> None:
+        raise NotImplementedError
+
+    def example(
+        self,
+        dataset_id: str,
+        example_id: str,
+        input_type: type[Input],
+        expected_output_type: type[ExpectedOutput],
+    ) -> Example[Input, ExpectedOutput] | None:
+        examples = self.examples(
+            dataset_id=dataset_id,
+            input_type=input_type,
+            expected_output_type=expected_output_type,
+        )
+
+        for example in examples:
+            if example.id == example_id:
+                return example
+        return None
+
+    def examples(
+        self,
+        dataset_id: str,
+        input_type: type[Input],
+        expected_output_type: type[ExpectedOutput],
+    ) -> Iterable[Example[Input, ExpectedOutput]]:
+
+        answers = "ABCD"
+        assert input_type == MultipleChoiceInput
+        assert expected_output_type == str
+        for index, sample in enumerate(self._huggingface_dataset["test"]):
+            yield Example(
+                input=cast(
+                    Input,
+                    MultipleChoiceInput(
+                        question=sample["question"], choices=sample["choices"]
+                    ),
+                ),
+                expected_output=cast(ExpectedOutput, answers[sample["answer"]]),
+                id=str(index),
+            )
---
+++ tests/evaluation/data_storage/test_single_huggingface_dataset_repository.py
@@ -0,0 +1,44 @@
+from datasets import load_dataset  # type: ignore
+
+from intelligence_layer.evaluation.data_storage.single_huggingface_dataset_repository import (
+    MultipleChoiceInput,
+    SingleHuggingfaceDatasetRepository,
+)
+
+
+def test_load_example_for_existing_dataset() -> None:
+
+    dataset = load_dataset(
+        path="hails/mmlu_no_train", name="all", trust_remote_code=True
+    )
+    repository = SingleHuggingfaceDatasetRepository(dataset)
+
+    examples = repository.examples(
+        dataset_id="", input_type=MultipleChoiceInput, expected_output_type=str
+    )
+
+    first_example = next(iter(examples))
+    assert (
+        first_example.input.question
+        == "Which of the following best describes the balance the Supreme Court has struck between the establishment clause and the free-exercise clause?"
+    )
+
+    assert first_example.input.choices == [
+        'Freedom of speech is protected except in certain situations, such as yelling "fire" in a crowded theater.',
+        "Once a church has been recognized by the federal government, its tax-exempt status can never be revoked.",
+        "Once Congress has created an administrative agency, that agency can be dissolved only by a constitutional amendment.",
+        "State-sponsored prayer during school hours is prohibited, but voluntary prayer by student groups before school is allowed.",
+    ]
+
+    assert first_example.expected_output == "D"
+
+    assert first_example.id == "0"
+
+    first_example_by_id = repository.example(
+        dataset_id="",
+        example_id=str(0),
+        input_type=MultipleChoiceInput,
+        expected_output_type=str,
+    )
+
+    assert first_example == first_example_by_id
