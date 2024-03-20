a626f21e6b100e661e03b52130ee700f47869665
Sebastian Zett <sebastian.zett@ext.aleph-alpha.com>
2024-03-20 10:22:49 +0100

    F13-35 do not skip unit tests anymore

--- tests/evaluation/test_graders.py
+++ tests/evaluation/test_graders.py
@@ -1,5 +1,5 @@
 import langdetect  # type: ignore
-from pytest import fixture, mark
+from pytest import fixture
 
 from intelligence_layer.evaluation.evaluation.graders import LanguageMatchesGrader
 
@@ -9,25 +9,21 @@ def set_deterministic_seed() -> None:
     langdetect.DetectorFactory.seed = 0
 
 
-@mark.skip("nltk download/load does not yet properly work in GitHub pipeline")
 def test_language_matches_grader_correctly_detects_languages_match() -> None:
     grader = LanguageMatchesGrader()
     assert grader.languages_match("This is a test.", "This is also a test.")
 
 
-@mark.skip("nltk download/load does not yet properly work in GitHub pipeline")
 def test_language_matches_grader_correctly_detects_languages_dont_match() -> None:
     grader = LanguageMatchesGrader()
     assert not grader.languages_match("This is a test.", "Dies ist noch ein Test.")
 
 
-@mark.skip("nltk download/load does not yet properly work in GitHub pipeline")
 def test_language_matches_grader_returns_true_if_input_language_is_unclear() -> None:
     grader = LanguageMatchesGrader()
     assert grader.languages_match("This is a test. Das ist ein Test.", "")
 
 
-@mark.skip("nltk download/load does not yet properly work in GitHub pipeline")
 def test_language_matches_grader_returns_false_if_output_language_is_unclear() -> None:
     grader = LanguageMatchesGrader()
     assert not grader.languages_match(
