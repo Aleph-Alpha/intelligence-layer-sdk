74b37cffc7810d841fda18aa4173311e96421a21
Sebastian Zett <sebastian.zett@ext.aleph-alpha.com>
2024-03-20 10:35:06 +0100

    F13-35 run all tests again

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env -S bash -eu -o pipefail
 
 poetry run python3 -c "import nltk; nltk.download('punkt')"
-#python3 -m nltk.downloader punkt
-poetry run pytest -n 10 tests/evaluation/test_graders.py
+poetry run pytest -n 10
