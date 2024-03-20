40ed8a93d481316b0cda8d1a73b50dce78ebfc74
Sebastian Zett <sebastian.zett@ext.aleph-alpha.com>
2024-03-20 10:29:29 +0100

    F13-35 download nltk before running pytest using -c

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env -S bash -eu -o pipefail
 
+poetry run python3 -c "import nltk; nltk.download('punkt')"
+#python3 -m nltk.downloader punkt
 poetry run pytest -n 10 tests/evaluation/test_graders.py
