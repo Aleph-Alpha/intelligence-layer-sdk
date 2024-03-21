c211137286f8281b60f8d91d950b9d05ad2607d2
Sebastian Zett <sebastian.zett@ext.aleph-alpha.com>
2024-03-21 15:13:26 +0100

    refactor HighlightCoverageGrader

--- tests/evaluation/test_graders.py
+++ tests/evaluation/test_graders.py
@@ -1,7 +1,7 @@
 import langdetect  # type: ignore
 from pytest import fixture
 
-from intelligence_layer.evaluation import LanguageMatchesGrader, HighlightCoverageGrader
+from intelligence_layer.evaluation import HighlightCoverageGrader, LanguageMatchesGrader
 
 
 @fixture(scope="session")
@@ -31,27 +31,95 @@ def test_language_matches_grader_returns_false_if_output_language_is_unclear() -
     )
 
 
-def test_highlight_coverage_grader_returns_perfect_score_for_good_highlights() -> None:
+def test_highlight_coverage_grader_returns_perfect_score_if_exact_match() -> None:
     grader = HighlightCoverageGrader()
-    text = "Hello, I am working on this highlight grader."
-    generated_highlights = {text[5:7], text[7:18]}
-    expected_highlights = {text[4:8], text[8:10], text[13:17]}
-    
-    assert grader.compare_highlights(
-        text=text,
-        generated_highlights=generated_highlights,
-        expected_highlights=expected_highlights
-    ) == 1.0
+    generated_highlights = [(3, 5)]
+    expected_highlights = [(3, 5)]
+
+    assert (
+        grader.compute_fscores(
+            generated_highlight_indices=generated_highlights,
+            expected_highlight_indices=expected_highlights,
+        ).f_score
+        == 1.0
+    )
 
 
-def test_highlight_coverage_grader_returns_null_score_for_all_text_highlighted() -> None:
+def test_highlight_coverage_grader_returns_worst_score_if_no_expected_highlight_identified() -> (
+    None
+):
     grader = HighlightCoverageGrader()
-    text = "Hello, I am working on this highlight grader."
-    generated_highlights = {text}
-    expected_highlights = {text[5:10]}
-    
-    assert grader.compare_highlights(
-        text=text,
-        generated_highlights=generated_highlights,
-        expected_highlights=expected_highlights
-    ) == 0.0
+    generated_highlights = [(0, 3)]
+    expected_highlights = [(3, 5)]
+
+    assert (
+        grader.compute_fscores(
+            generated_highlight_indices=generated_highlights,
+            expected_highlight_indices=expected_highlights,
+        ).f_score
+        == 0.0
+    )
+
+
+def test_highlight_coverage_grader_returns_perfect_score_if_highlights_exactly_identified_but_split_up() -> (
+    None
+):
+    grader = HighlightCoverageGrader()
+    generated_highlights = [(3, 4), (4, 7)]
+    expected_highlights = [(3, 7)]
+
+    assert (
+        grader.compute_fscores(
+            generated_highlight_indices=generated_highlights,
+            expected_highlight_indices=expected_highlights,
+        ).f_score
+        == 1.0
+    )
+
+
+def test_highlight_coverage_grader_returns_perfect_score_if_highlights_exactly_identified_but_merged() -> (
+    None
+):
+    grader = HighlightCoverageGrader()
+    generated_highlights = [(3, 6)]
+    expected_highlights = [(3, 5), (5, 6)]
+
+    assert (
+        grader.compute_fscores(
+            generated_highlight_indices=generated_highlights,
+            expected_highlight_indices=expected_highlights,
+        ).f_score
+        == 1.0
+    )
+
+
+def test_highlight_coverage_grader_returns_perfect_score_if_all_but_more_highlights_identified_and_beta_equals_zero() -> (
+    None
+):
+    grader = HighlightCoverageGrader(beta_factor=0)
+    generated_highlights = [(3, 5)]
+    expected_highlights = [(3, 6)]
+
+    assert (
+        grader.compute_fscores(
+            generated_highlight_indices=generated_highlights,
+            expected_highlight_indices=expected_highlights,
+        ).f_score
+        == 1.0
+    )
+
+
+def test_highlight_coverage_grader_returns_nonzero_nonperfect_score_with_some_false_negatives_and_some_false_positives() -> (
+    None
+):
+    grader = HighlightCoverageGrader()
+    generated_highlights = [(3, 5)]
+    expected_highlights = [(4, 7)]
+
+    assert (
+        grader.compute_fscores(
+            generated_highlight_indices=generated_highlights,
+            expected_highlight_indices=expected_highlights,
+        ).f_score
+        == 0.4
+    )
