18f6f5d7c141f5fdd226ca561a9cd6b1388f20db
SebastianWolfschmidtAA <162735941+SebastianWolfschmidtAA@users.noreply.github.com>
2024-03-27 11:24:54 +0100

    F13 67 fix language detection (#665)
    * F13-67 created  method for tokenizing text for better flexibility
    
    * F13-67 added new test to language grader to test for difficult input
    
    * F13-67 catch expection from langdetect if language cannot be determined
    
    * F13-67 fixed linting and return types
    
    * F13-67 changed tests to better reflect relevant sentence length
    
    * F13-67 changed method of chunking text into blocks to avoid exceptions
    
    * F13-67 fixed typo
    
    * F13-67 removed unused import
    
    * F13-67 new test for empty input, refactoring
    
    * fix(tests): autouse pytest fixture to correctly set value of langdetect seed for all unit tests
    
    ---------
    
    Co-authored-by: Sebastian Zett <sebastian.zett@ext.aleph-alpha.com>

--- tests/evaluation/test_graders.py
+++ tests/evaluation/test_graders.py
@@ -4,30 +4,56 @@ from pytest import fixture
 from intelligence_layer.evaluation import HighlightCoverageGrader, LanguageMatchesGrader
 
 
-@fixture(scope="session")
+@fixture(scope="session", autouse=True)
 def set_deterministic_seed() -> None:
     langdetect.DetectorFactory.seed = 0
 
 
 def test_language_matches_grader_correctly_detects_languages_match() -> None:
     grader = LanguageMatchesGrader()
-    assert grader.languages_match("This is a test.", "This is also a test.")
+    assert grader.languages_match(
+        "This is a test of a sentence in one language.",
+        "This is also a test of another sentence in the same language.",
+    )
 
 
 def test_language_matches_grader_correctly_detects_languages_dont_match() -> None:
     grader = LanguageMatchesGrader()
-    assert not grader.languages_match("This is a test.", "Dies ist noch ein Test.")
+    assert not grader.languages_match(
+        "This is a test of a sentence in one language.",
+        "Dies ist ein weiterer Test eines anderen Satzes in einer anderen Sprache.",
+    )
 
 
 def test_language_matches_grader_returns_true_if_input_language_is_unclear() -> None:
     grader = LanguageMatchesGrader()
-    assert grader.languages_match("This is a test. Das ist ein Test.", "")
+    assert grader.languages_match(
+        "This is a test of a sentence in one language. Das ist ein Test eines anderen Satzes in einer anderen Sprache.",
+        "",
+    )
 
 
 def test_language_matches_grader_returns_false_if_output_language_is_unclear() -> None:
     grader = LanguageMatchesGrader()
     assert not grader.languages_match(
-        "This is a test.", "This is a test. Das ist ein Test."
+        "This is a test of a sentence in one language.",
+        "This is a test of a sentence in one language. Das ist ein Test eines anderen Satzes in einer anderen Sprache.",
+    )
+
+
+def test_language_matches_grader_can_handle_difficult_input() -> None:
+    grader = LanguageMatchesGrader()
+    assert grader.languages_match(
+        "1. This test is difficult. 2. And it has esp. diff. characters.",
+        "a) Here is a another test.\nb) How can it handle enumerations?",
+    )
+
+
+def test_language_matches_grader_empty_input_and_output() -> None:
+    grader = LanguageMatchesGrader()
+    assert grader.languages_match(
+        "",
+        "",
     )
 
 
