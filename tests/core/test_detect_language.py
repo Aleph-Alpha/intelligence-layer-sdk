e86e5059d06ed212e882a285823fecc3fdf04c5d
Sebastian Wolfschmidt <sebastian.wolfschmidt@ext.aleph-alpha.com>
2024-04-17 14:54:42 +0200

    add test for case where language cannot be detected

--- tests/core/test_detect_language.py
+++ tests/core/test_detect_language.py
@@ -17,3 +17,16 @@ def test_detect_language_returns_correct_language() -> None:
     output = task.run(input, tracer)
 
     assert output.best_fit == Language("en")
+
+
+def test_detect_language_returns_non_if_no_language_can_be_detected() -> None:
+    text = "Je m’appelle Jessica. Je suis une fille, je suis française et j’ai treize ans."  # codespell:ignore
+    task = DetectLanguage()
+    input = DetectLanguageInput(
+        text=text,
+        possible_languages=[Language(lang) for lang in ["en", "de"]],
+    )
+    tracer = NoOpTracer()
+    output = task.run(input, tracer)
+
+    assert output.best_fit is None
