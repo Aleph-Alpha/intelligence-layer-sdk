79e67392a79761e9e43e09a0c8084f2ec42df581
Martin Achtner <martin.achtner@ext.aleph-alpha.com>
2024-04-19 15:33:07 +0200

    feat: Add safety classification tests for longer text

--- tests/core/test_safety_classifier.py
+++ tests/core/test_safety_classifier.py
@@ -23,6 +23,14 @@ def safety_classifier(
     return SafetyClassifier(model=None)
 
 
+@fixture
+def long_text() -> str:
+    return """Green Day is an American rock band formed in the East Bay of California in 1987 by lead vocalist and guitarist Billie Joe Armstrong, together with bassist and backing vocalist Mike Dirnt. For most of the band's career they have been a power trio[4] with drummer Tré Cool, who replaced John Kiffmeyer in 1990 before the recording of the band's second studio album, Kerplunk (1991). Before taking its current name in 1989, Green Day was called Blood Rage, then Sweet Children and they were part of the late 1980s/early 1990s Bay Area punk scene that emerged from the 924 Gilman Street club in Berkeley, California. The band's early releases were with the independent record label Lookout! Records. In 1994, their major-label debut Dookie, released through Reprise Records, became a breakout success and eventually shipped over 10 million copies in the U.S. Alongside fellow California punk bands Bad Religion, the Offspring, Rancid, NOFX, Pennywise and Social Distortion, Green Day is credited with popularizing mainstream interest in punk rock in the U.S.
+Though the albums Insomniac (1995), Nimrod (1997) and Warning (2000) did not match the success of Dookie, they were still successful, with the first two reaching double platinum status, while the last achieved gold. Green Day's seventh album, a rock opera called American Idiot (2004), found popularity with a younger generation, selling six million copies in the U.S. Their next album, 21st Century Breakdown, was released in 2009 and achieved the band's best chart performance. It was followed by a trilogy of albums, ¡Uno!, ¡Dos!, and ¡Tré!, released in September, November, and December 2012, respectively. The trilogy did not perform as well as expected commercially, in comparison to their previous albums, largely due to a lack of promotion and Armstrong entering rehab. Their twelfth studio album, Revolution Radio, was released in October 2016 and became their third to debut at No. 1 on the Billboard 200. The band's thirteenth studio album, Father of All Motherfuckers, was released on February 7, 2020. The band's fourteenth studio album, titled Saviors, is planned to come out on January 19, 2024.[5][6]
+In 2010, a stage adaptation of American Idiot debuted on Broadway. The musical was nominated for three Tony Awards: Best Musical, Best Scenic Design and Best Lighting Design, winning the latter two. The band was inducted into the Rock and Roll Hall of Fame in 2015, their first year of eligibility. Members of the band have collaborated on the side projects Pinhead Gunpowder, The Network, Foxboro Hot Tubs, The Longshot and The Coverups. They have also worked on solo careers.
+Green Day has sold roughly 75 million records worldwide,[7][unreliable source?] making them one of the world's best-selling artists. The group has been nominated for 20 Grammy awards and has won five of them with Best Alternative Album for Dookie, Best Rock Album for American Idiot and 21st Century Breakdown, Record of the Year for "Boulevard of Broken Dreams" and Best Musical Show Album for American Idiot: The Original Broadway Cast Recording."""
+
+
 def test_safety_classifier_returns_no_flags_for_safe_input(
     safety_classifier: SafetyClassifier,
 ) -> None:
@@ -77,3 +85,24 @@ def test_safety_classifier_returns_flags_for_unsafe_german_input(
 
     output = safety_classifier.run(input, NoOpTracer())
     assert output.flags == expected_labels
+
+
+def test_safety_classifier_returns_flags_for_safe_long_input(
+    safety_classifier: SafetyClassifier, long_text: str
+) -> None:
+    input = SafetyClassifyInput(text=TextChunk(long_text), language=Language("en"))
+
+    output = safety_classifier.run(input, NoOpTracer())
+    assert len(output.flags) == 0
+
+
+def test_safety_classifier_returns_flags_for_unsafe_long_input(
+    safety_classifier: SafetyClassifier, long_text: str
+) -> None:
+    unsafe_text = "\nThe band is know for their history of open drug abuse and has made some headline about violence towards minors."
+    input = SafetyClassifyInput(
+        text=TextChunk(long_text + unsafe_text), language=Language("en")
+    )
+
+    output = safety_classifier.run(input, NoOpTracer())
+    assert len(output.flags) == 1
