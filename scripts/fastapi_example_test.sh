07236c3554fbefcb6a9537dad535aca28ee92df1
Niklas Köhnecke <niklas.koehnecke@ext.aleph-alpha.com>
2024-04-25 16:32:42 +0200

    fix: improve waiting logic

--- scripts/fastapi_example_test.sh
+++ scripts/fastapi_example_test.sh
@@ -2,11 +2,24 @@
 
 # start the server in the background
 hypercorn src/examples/fastapi_example:app --bind localhost:8000 &
-my_pid=$!
+server_pid=$!
 
-trap 'kill $my_pid' EXIT SIGINT
+attempt_counter=0
+max_attempts=10
+
+trap 'kill $server_pid' EXIT SIGINT
 # waiting for server startup
-sleep 3
+until $(curl -X GET http://localhost:8000 --fail-with-body --output /dev/null --silent --head); do
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+      echo "Max attempts reached"
+      exit 1
+    fi
+
+    printf '.'
+    attempt_counter=$(($attempt_counter+1))
+    sleep 1
+done
+
 curl -X GET http://localhost:8000 --fail-with-body
 curl -X POST http://localhost:8000/summary --fail-with-body -H "Content-Type: application/json" -d '{"chunk": "<your text to summarize here>", "language": {"iso_639_1": "en"}}'
 
