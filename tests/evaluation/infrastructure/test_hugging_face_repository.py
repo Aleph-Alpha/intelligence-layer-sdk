30b144c85f9e5c5c10a21ca62ad5bc5212dc811e
Niklas Köhnecke <niklas.koehnecke@ext.aleph-alpha.com>
2024-06-04 18:43:49 +0200

    fix: improve test stability for hf tests

--- tests/evaluation/infrastructure/test_hugging_face_repository.py
+++ tests/evaluation/infrastructure/test_hugging_face_repository.py
@@ -8,13 +8,13 @@ from intelligence_layer.evaluation.infrastructure.hugging_face_repository import
 def test_hugging_face_repository_can_create_and_delete_a_repository(
     hugging_face_token: str, hugging_face_test_repository_id: str
 ) -> None:
-    repository_id = hugging_face_test_repository_id
+    repository_id = hugging_face_test_repository_id + "unused-suffix"
 
     assert not huggingface_hub.repo_exists(
         repo_id=repository_id,
         token=hugging_face_token,
         repo_type="dataset",
-    ), f"This is very unlikely but it seems that the repository with the ID {repository_id} already exists."
+    ), f"The repository with the ID {repository_id} already exists. Try to run the clean_hf script."
 
     created_repository = HuggingFaceRepository(
         repository_id=repository_id,
