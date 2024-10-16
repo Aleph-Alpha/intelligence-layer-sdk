819f542728ec4fbf809c13338595acf976c37867
Niklas Köhnecke <niklas.koehnecke@ext.aleph-alpha.com>
2024-10-16 13:34:54 +0200

    initial version of otel with collector

--- src/intelligence_layer/examples/opentelemetry/receive_traces.py
+++ src/intelligence_layer/examples/opentelemetry/receive_traces.py
@@ -7,11 +7,11 @@ import json
 
 app = FastAPI()
 
+
 @app.post("/print_json")
 async def print_json(request: Request):
     json_data = await request.json()
-    with open('test.jsonl', 'a') as file:
-        file.write(str(datetime.now()) + '\t' + json.dumps(json_data) + '\n')
+    print("received")
+    with open("test.jsonl", "a") as file:
+        file.write(str(datetime.now()) + "\t" + json.dumps(json_data) + "\n")
     return {"message": "JSON received and logged"}
-
-
--- src/intelligence_layer/examples/opentelemetry/send_traces.py
+++ src/intelligence_layer/examples/opentelemetry/send_traces.py
@@ -1,6 +1,5 @@
 import logging
 import time
-from asyncio import sleep
 from typing import Sequence
 from uuid import uuid4
 
@@ -8,27 +7,29 @@ import requests
 from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-from opentelemetry.sdk.trace import TracerProvider, ReadableSpan
+from opentelemetry.sdk.trace import ReadableSpan, TracerProvider
 from opentelemetry.sdk.trace.export import (
-    SimpleSpanProcessor, SpanExporter, SpanExportResult, BatchSpanProcessor,
+    BatchSpanProcessor,
+    SpanExporter,
+    SpanExportResult,
 )
-from sentry_sdk.utils import json_dumps
+from tqdm import tqdm
 
 from intelligence_layer.core import OpenTelemetryTracer, Task
 from intelligence_layer.core.tracer.tracer import TaskSpan
 
 logging.basicConfig(level=logging.DEBUG)
 
-class StudioSpanExporter(SpanExporter):
 
+class StudioSpanExporter(SpanExporter):
     def __init__(self):
         pass
 
     def export(self, spans: Sequence[ReadableSpan]) -> SpanExportResult:
-        url = 'http://127.0.0.1:8000/print_json'
+        url = "http://127.0.0.1:8000/print_json"
         json_spans = [span.to_json() for span in spans]
         print(json_spans)
-        requests.post(url, '[' + ','.join(json_spans) + ']')
+        requests.post(url, "[" + ",".join(json_spans) + "]")
 
         return SpanExportResult.SUCCESS
 
@@ -37,6 +38,7 @@ class StudioSpanExporter(SpanExporter):
 
     def force_flush(self, timeout_millis=30000):
         pass
+
 
 class TracerTestSubTask(Task[None, None]):
     def do_run(self, input: None, task_span: TaskSpan) -> None:
@@ -47,17 +49,17 @@ class TracerTestTask(Task[str, str]):
     sub_task = TracerTestSubTask()
 
     def do_run(self, input: str, task_span: TaskSpan) -> str:
-        time.sleep(0.001)
+        sleep = 0.1
+        time.sleep(sleep)
         with task_span.span("span") as sub_span:
-            time.sleep(0.001)
+            time.sleep(sleep)
             sub_span.log("message", "a value")
-            time.sleep(0.001)
+            time.sleep(sleep)
             self.sub_task.run(None, sub_span)
-            time.sleep(0.001)
+            time.sleep(sleep)
         self.sub_task.run(None, task_span)
-        time.sleep(0.001)
+        time.sleep(sleep)
         return "output"
-
 
 
 project_name = str(uuid4())
@@ -67,12 +69,12 @@ resource = Resource.create({SERVICE_NAME: service_name})
 provider = TracerProvider(resource=resource)
 trace.set_tracer_provider(provider)
 
-url = 'http://localhost:4318'
+url = "http://localhost:4318/v1/traces"
 processor = BatchSpanProcessor(OTLPSpanExporter(url), max_export_batch_size=10)
 
 provider.add_span_processor(processor)
 tracer = OpenTelemetryTracer(trace.get_tracer("intelligence-layer"))
 
-for i in range(1):
+for i in tqdm(range(1000), total=1000):
     input_value = {"key": "value"}
     TracerTestTask().run(input_value, tracer)
