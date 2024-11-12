76c07fa51c872f217792a3b9e1fa327ab6113b4f
Johannes Wesch <johannes.wesch@aleph-alpha.com>
2024-11-12 09:19:56 +0100

    feat: add test for benchmark

--- tests/evaluation/benchmark/test_benchmark.py
+++ tests/evaluation/benchmark/test_benchmark.py
@@ -1,6 +1,13 @@
+from datetime import datetime
 from uuid import UUID, uuid4
 
-from intelligence_layer.connectors.studio.studio import StudioClient
+import pytest
+from pytest import fixture
+
+from intelligence_layer.connectors.studio.studio import (
+    GetBenchmarkResponse,
+    StudioClient,
+)
 from intelligence_layer.evaluation.benchmark.studio_benchmark import (
     StudioBenchmarkRepository,
     create_aggregation_logic_identifier,
@@ -12,9 +19,46 @@ from tests.evaluation.conftest import (
 )
 
 
-def test_extract_types_from_eval_logic() -> None:
+@fixture
+def datatset_id() -> str:
+    return "dataset_id"
+
+
+@fixture
+def get_benchmark_response(datatset_id: str) -> GetBenchmarkResponse:
     eval_logic = DummyEvaluationLogic()
-    created_identifier = create_evaluation_logic_identifier(eval_logic)
+    evaluation_identifier = create_evaluation_logic_identifier(eval_logic)
+    aggregation_logic = DummyAggregationLogic()
+    aggregation_identifier = create_aggregation_logic_identifier(aggregation_logic)
+    return GetBenchmarkResponse(
+        id="id",
+        project_id=0,
+        dataset_id=datatset_id,
+        name="name",
+        description="description",
+        benchmark_metadata=None,
+        evaluation_logic=evaluation_identifier,
+        aggregation_logic=aggregation_identifier,
+        created_at=datetime.now(),
+        updated_at=None,
+        last_executed_at=None,
+        created_by=None,
+        updated_by=None,
+    )
+
+
+@fixture
+def evaluation_logic() -> DummyEvaluationLogic:
+    return DummyEvaluationLogic()
+
+
+@fixture
+def aggregation_logic() -> DummyAggregationLogic:
+    return DummyAggregationLogic()
+
+
+def test_extract_types_from_eval_logic(evaluation_logic: DummyEvaluationLogic) -> None:
+    created_identifier = create_evaluation_logic_identifier(evaluation_logic)
     dummy_logic = "if output == FAIL_IN_EVAL_INPUT:"
     dummy_type = "DummyEvaluation"
     assert dummy_logic in created_identifier.logic
@@ -29,8 +73,9 @@ def test_extract_types_from_eval_logic() -> None:
     )
 
 
-def test_extract_types_from_aggregation_logic() -> None:
-    aggregation_logic = DummyAggregationLogic()
+def test_extract_types_from_aggregation_logic(
+    aggregation_logic: DummyAggregationLogic,
+) -> None:
     created_identifier = create_aggregation_logic_identifier(aggregation_logic)
     dummy_logic = "return DummyAggregation(num_evaluations=len(list(evaluations)))"
 
@@ -42,14 +87,14 @@ def test_extract_types_from_aggregation_logic() -> None:
 def test_create_benchmark(
     studio_benchmark_repository: StudioBenchmarkRepository,
     mock_studio_client: StudioClient,
+    evaluation_logic: DummyEvaluationLogic,
+    aggregation_logic: DummyAggregationLogic,
 ) -> None:
-    eval_logic = DummyEvaluationLogic()
-    aggregation_logic = DummyAggregationLogic()
     dataset_id = "fake_dataset_id"
     mock_studio_client.create_benchmark.return_value = str(uuid4())  # type: ignore
 
     benchmark = studio_benchmark_repository.create_benchmark(
-        dataset_id, eval_logic, aggregation_logic, "benchmark_name"
+        dataset_id, evaluation_logic, aggregation_logic, "benchmark_name"
     )
     uuid = UUID(benchmark.id)
     assert uuid
@@ -60,26 +105,32 @@ def test_create_benchmark(
 def test_get_benchmark(  # TODO
     studio_benchmark_repository: StudioBenchmarkRepository,
     mock_studio_client: StudioClient,
+    get_benchmark_response: GetBenchmarkResponse,
+    evaluation_logic: DummyEvaluationLogic,
+    aggregation_logic: DummyAggregationLogic,
+    datatset_id: str,
 ) -> None:
-    eval_logic = DummyEvaluationLogic()
-    aggregation_logic = DummyAggregationLogic()
-    mock_studio_client.create_benchmark.return_value = str(uuid4())  # type: ignore
+    benchmark_id = "benchmark_id"
+    mock_studio_client.get_benchmark.return_value = get_benchmark_response  # type: ignore
 
     benchmark = studio_benchmark_repository.get_benchmark(
-        "benchmark_id", eval_logic, aggregation_logic
+        benchmark_id, evaluation_logic, aggregation_logic
     )
-    assert benchmark.id == "benchmark_id"
-    assert benchmark.dataset_id == "dataset_id"
+    assert benchmark.id == benchmark_id
+    assert benchmark.dataset_id == datatset_id
     assert benchmark.eval_logic
     assert benchmark.aggregation_logic
 
 
 def test_get_non_existing_benchmark(  # TODO
     studio_benchmark_repository: StudioBenchmarkRepository,
+    mock_studio_client: StudioClient,
+    evaluation_logic: DummyEvaluationLogic,
+    aggregation_logic: DummyAggregationLogic,
 ) -> None:
-    eval_logic = DummyEvaluationLogic()
-    aggregation_logic = DummyAggregationLogic()
+    mock_studio_client.get_benchmark.return_value = None  # type: ignore
 
-    studio_benchmark_repository.get_benchmark(
-        "non_existing_id", eval_logic, aggregation_logic
-    )
+    with pytest.raises(ValueError):
+        studio_benchmark_repository.get_benchmark(
+            "non_existing_id", evaluation_logic, aggregation_logic
+        )
